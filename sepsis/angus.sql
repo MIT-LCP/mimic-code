f100dc245f91c2ecd9247a199bba70103217960d
Tom Pollard <tpollard@mit.edu>
2015-09-26 17:25:39 -0400

    add sepsis code

---
+++ sepsis/angus.sql
@@ -0,0 +1,11 @@
+-- ICD-9 codes for Angus criteria of sepsis
+
+-- Angus et al, 2001. Epidemiology of severe sepsis in the United States
+-- http://www.ncbi.nlm.nih.gov/pubmed/11445675
+
+-- Case selection and definitions
+-- To identify cases with severe sepsis, we selected all acute care 
+-- hospitalizations with ICD-9-CM codes for both:
+-- (a) a bacterial or fungal infectious process AND 
+-- (b) a diagnosis of acute organ dysfunction (Appendix 2). 
+
