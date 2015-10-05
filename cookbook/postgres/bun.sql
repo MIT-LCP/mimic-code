5fc658779a68cca5546c0ba2322f1c60558ecb16
erinhong <erin.hong17@gmail.com>
2015-10-05 14:21:37 -0400

    bun.sql

---
+++ cookbook/postgres/bun.sql
@@ -0,0 +1,25 @@
+-- retrieves the amount of billirubun of each adult patient found in the patients table 
+
+with agetbl as
+(
+	select ad.subject_id, ad.hadm_id
+       from mimiciii.admissions ad
+       inner join mimiciii.patients p
+       on ad.subject_id = p.subject_id 
+       where
+       -- filter to only adults
+        ( 
+		(extract(DAY from ad.admittime - p.dob) 
+			+ extract(HOUR from ad.admittime - p.dob) /24
+			+ extract(MINUTE from ad.admittime - p.dob) / 24 / 60
+			) / 365.25 
+	) > 15
+)
+select bucket, count(*) from (
+  select width_bucket(valuenum, 0, 280, 280) as bucket
+    from mimiciii.labevents le,
+    inner join agetbl 
+    on le.subject_id = agetbl.subject_id
+   where itemid in (51006)
+  ) as bun
+  group by bucket order by bucket;
