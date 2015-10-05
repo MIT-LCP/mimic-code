ad8aa3c9e4808f985d3b846e3f49536ad3ebfe11
erinhong <erin.hong17@gmail.com>
2015-10-05 14:29:12 -0400

    Create heart_rate.sql

---
+++ cookbook/postgres/heart_rate.sql
@@ -0,0 +1,27 @@
+-- retrieves the heart rate of adult patients 
+-- only for patients recorded with carevue 
+
+with agetbl as
+(
+	select ad.subject_id, ad.hadm_id
+       from mimiciii.admissions ad
+       inner join mimiciii.patients p
+       on ad.subject_id = p.subject_id 
+       where
+       -- filter to only adults
+        ( 
+		(extract(DAY from ad.admittime - p.dob) 
+			+ extract(HOUR from ad.admittime - p.dob) /24
+			+ extract(MINUTE from ad.admittime - p.dob) / 24 / 60
+			) / 365.25 
+	) > 15
+)
+
+select bucket, count(*) from (
+  select width_bucket(valuenum, 0, 300, 301) as bucket
+    from mimiciii.chartevents ce
+    inner join agetbl 
+    on ce.subject_id = agetbl.subject_id
+   where itemid = 211
+   )as heart_rate
+   group by bucket order by bucket;
