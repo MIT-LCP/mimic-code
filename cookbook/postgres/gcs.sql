c6bd011bb3de309e41444a38d85f1029a2a273c8
erinhong <erin.hong17@gmail.com>
2015-10-05 14:23:29 -0400

    Create gcs.sql

---
+++ cookbook/postgres/gcs.sql
@@ -0,0 +1,26 @@
+-- retrieves the glasgow coma score for each adult patient 
+-- only works for patients recorded with carevue 
+
+with agetbl as
+(
+	select ad.subject_id, ad.hadm_id
+       from mimiciii.admissions ad
+       inner join mimiciii.patients p
+       on ad.subject_id = p.subject_id 
+       where
+       -- filter to only adults
+        ( 
+		(extract(DAY from ad.admittime - p.dob) 
+			+ extract(HOUR from ad.admittime - p.dob) /24
+			+ extract(MINUTE from ad.admittime - p.dob) / 24 / 60
+			) / 365.25 
+	) > 15
+)
+select bucket, count(*) from (
+  select width_bucket(valuenum, 1, 30, 30) as bucket
+    from mimiciii.chartevents ce
+    inner join agetbl    
+    on ce.subject_id = agetbl.subject_id
+   where itemid in (198)
+       )as gcs
+       group by bucket order by bucket;
