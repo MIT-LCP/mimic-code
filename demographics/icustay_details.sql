0b6e5b707be2016f81f94029d14f3c356185aea8
Alistair Johnson <alistairewj@gmail.com>
2015-11-24 09:21:59 -0500

    updated interval calculations to use epoch

--- demographics/icustay_details.sql
+++ demographics/icustay_details.sql
@@ -11,28 +11,32 @@
 
 with co as (
 select ie.subject_id, ie.hadm_id, ie.icustay_id
-​
+
 -- patient level factors
 , pat.gender
-
 -- hospital level factors
 , adm.ethnicity
 , adm.ADMISSION_TYPE
 , adm.admittime, adm.dischtime
-, case when adm.deathtime is not null then 'Y' else 'N' end
-          as hospital_expire_flag
+, case 
+    when adm.deathtime is not null then 'Y' 
+    else 'N' 
+  end
+  as hospital_expire_flag
 , row_number() over (partition by ie.subject_id, ie.hadm_id order by ie.intime) as hospstay_num
-, case when row_number() over (partition by ie.subject_id, ie.hadm_id order by ie.intime) = 1 then 'Y' else 'N' end
+, case 
+    when row_number() over (partition by ie.subject_id, ie.hadm_id order by ie.intime) = 1 then 'Y' 
+    else 'N' 
+  end
 	as first_hosp_stay
-
+  
 -- icu level factors
 , ie.intime, ie.outtime
-, EXTRACT(DAY FROM ie.intime-pat.dob) / 365.25 as Age
-, round(ie.outtime - ie.intime,4) as LOS_ICU
+, round(EXTRACT(EPOCH FROM (ie.intime-pat.dob)) / 60 / 60 / 24 / 365.242, 4) as Age
+, round(EXTRACT(EPOCH FROM (ie.outtime - ie.intime)) / 60 / 60 / 24, 4) as LOS_ICU
 , row_number() over (partition by ie.subject_id, ie.hadm_id order by ie.intime) as icustay_num
-​
-​
-from mimiciii.icustayevents ie
+
+from mimiciii.icustays ie
 inner join mimiciii.admissions adm
  on ie.hadm_id = adm.hadm_id
 inner join mimiciii.patients pat
