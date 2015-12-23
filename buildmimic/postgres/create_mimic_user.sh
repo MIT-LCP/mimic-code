7c703fd25c357506e4172441b9cbec85af6c0455
Eric Carlson <eric.carlson@philips.com>
2015-12-23 17:18:07 -0500

    Reverting to pre-change to see if master passes checks

--- buildmimic/postgres/create_mimic_user.sh
+++ buildmimic/postgres/create_mimic_user.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-if [ -z ${MIMIC_PASSWORD+x} ]; then
-  echo "MIMIC_PASSWORD is unset";
-  exit 1
-else
-  echo "MIMIC_PASSWORD is set";
-fi
-
-if [ -z ${MIMIC_DB+x} ]; then
-  MIMIC_DB=mimic
-  echo "MIMIC_DB is unset, using default '$MIMIC_DB'";
-else
-  echo "MIMIC_DB is set to '$MIMIC_DB'";
-fi
-
-if [ -z ${MIMIC_USER+x} ]; then
-  MIMIC_USER=MIMIC
-  echo "MIMIC_USER is unset, using default '$MIMIC_USER'";
-else
-  echo "MIMIC_USER is set to '$MIMIC_USER'";
-fi
-
-if hash gosu 2>/dev/null; then
-    SUDO='gosu postgres'
-else
-    SUDO='sudo -u postgres'
-fi
-
-$SUDO psql > /dev/null <<- EOSQL
-    CREATE USER MIMIC WITH PASSWORD '$MIMIC_PASSWORD';
-    CREATE DATABASE $MIMIC_DB OWNER $MIMIC_USER;
-    \c $MIMIC_DB;
-    CREATE SCHEMA MIMICIII;
-    ALTER SCHEMA MIMICIII OWNER TO $MIMIC_USER;
+gosu postgres psql > /dev/null <<- EOSQL
+                CREATE USER MIMIC WITH PASSWORD '$MIMIC_PASSWORD';
+                CREATE DATABASE MIMIC OWNER MIMIC;
+                \c mimic;
+                CREATE SCHEMA MIMICIII;
+		ALTER SCHEMA MIMICIII OWNER TO MIMIC;
 EOSQL
