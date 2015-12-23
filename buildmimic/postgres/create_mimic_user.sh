e82dc42c0502e29200ee8a59f78bd7c1d9e759af
Eric Carlson <eric.carlson@philips.com>
2015-12-23 16:22:35 -0500

    Remove dependency on "gosu" by using standard "sudo" with user option

--- buildmimic/postgres/create_mimic_user.sh
+++ buildmimic/postgres/create_mimic_user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gosu postgres psql > /dev/null <<- EOSQL
+sudo -u postgres psql > /dev/null <<- EOSQL
                 CREATE USER MIMIC WITH PASSWORD '$MIMIC_PASSWORD';
                 CREATE DATABASE MIMIC OWNER MIMIC;
 EOSQL
