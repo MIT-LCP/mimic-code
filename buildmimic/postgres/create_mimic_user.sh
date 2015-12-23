437caf0b446147d88b0793fec9aa448818e089c0
Eric Carlson <eric.carlson@philips.com>
2015-12-23 16:21:27 -0500

    Schema is already being created in 'create_tables', remove here to reduce what's needed to be done by postgres administrator

--- buildmimic/postgres/create_mimic_user.sh
+++ buildmimic/postgres/create_mimic_user.sh
@@ -3,7 +3,4 @@
 gosu postgres psql > /dev/null <<- EOSQL
                 CREATE USER MIMIC WITH PASSWORD '$MIMIC_PASSWORD';
                 CREATE DATABASE MIMIC OWNER MIMIC;
-                \c mimic;
-                CREATE SCHEMA MIMICIII;
-		ALTER SCHEMA MIMICIII OWNER TO MIMIC;
 EOSQL
