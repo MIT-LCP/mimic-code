32b31c5631e687e9a8934198c966db4fb91f9517
Eric Carlson <eric.carlson@philips.com>
2015-12-23 16:24:33 -0500

    Adding checking for variable existance, use of defaults if not set

--- buildmimic/postgres/create_mimic_user.sh
+++ buildmimic/postgres/create_mimic_user.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+if [ -z ${MIMIC_PASSWORD+x} ]; then
+  echo "MIMIC_PASSWORD is unset";
+  exit 1
+else
+  echo "MIMIC_PASSWORD is set";
+fi
+
+if [ -z ${MIMIC_DB+x} ]; then
+  MIMIC_DB=MIMIC
+  echo "MIMIC_DB is unset, using default '$MIMIC_DB'";
+else
+  echo "MIMIC_DB is set to '$MIMIC_DB'";
+fi
+
+if [ -z ${MIMIC_USER+x} ]; then
+  MIMIC_USER=MIMIC
+  echo "MIMIC_USER is unset, using default '$MIMIC_USER'";
+else
+  echo "MIMIC_USER is set to '$MIMIC_USER'";
+fi
+
 sudo -u postgres psql > /dev/null <<- EOSQL
                 CREATE USER MIMIC WITH PASSWORD '$MIMIC_PASSWORD';
-                CREATE DATABASE MIMIC OWNER MIMIC;
+                CREATE DATABASE $MIMIC_DB OWNER $MIMIC_USER;
 EOSQL
