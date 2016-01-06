cf2fdeb914bd1d237d38a8f50f6f7d47bfaa2adc
Mike Wu <mike.wu@yale.edu>
2016-01-06 13:06:48 -0800

    Update height.sql
    Fixed a typo "value1num --> valuenum"

--- cookbook/postgres/height.sql
+++ cookbook/postgres/height.sql
@@ -1,7 +1,7 @@
 -- retrieves a height histogram of patients 
 
 select bucket, count(*) from (
-select valuenum, width_bucket(value1num, 1, 200, 200) as
+select valuenum, width_bucket(valuenum, 1, 200, 200) as
 bucket from mimiciii.chartevents where itemid = 920 and valuenum is
 not null and valuenum > 0 and valuenum < 500
       )as height 
