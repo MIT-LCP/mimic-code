d905df26d1151bf6dafba819ab386243fb97332c
Alistair Johnson <alistairewj@gmail.com>
2016-01-28 20:13:01 -0500

    refactored, added some concepts

--- demographics/postgres/icustay_detail.sql
+++ demographics/postgres/icustay_detail.sql
@@ -3,53 +3,45 @@
 -- Description: This query provides a useful set of information regarding patient
 --			ICU stays. The information is combined from the admissions, patients, and
 --			icustays tables. It includes age, length of stay, sequence, and expiry flags.
--- MIMIC version: MIMIC-III v1.2
--- Created by: Erin Hong, Alistair Johnson
--- Editted by: Marzyeh Ghassemi
+-- MIMIC version: MIMIC-III v1.4
 -- ------------------------------------------------------------------
 
--- Define which schema to work on
-SET search_path TO mimiciii;
+-- (Optional) Define which schema to work on
+-- SET search_path TO mimiciii;
 
 -- This query extracts useful demographic/administrative information for patient ICU stays
-with adm as (
-select ad.subject_id, ad.hadm_id, 
-ad.ethnicity, ad.ADMISSION_TYPE, ad.admittime, ad.dischtime,
-case 
-  when ad.deathtime is not null then 'Y' 
-  else 'N' 
-end
-as hospital_expire_flag,
-row_number() over (partition by ad.subject_id order by ad.admittime) as hospstay_seq,
-case 
-  when row_number() over (partition by ad.subject_id order by ad.admittime) = 1 then 'Y' 
-  else 'N' 
-end
-as first_hosp_stay
-
-from admissions ad
-where ad.has_chartevents_data = 1
-)
-,
-co as (
 select ie.subject_id, ie.hadm_id, ie.icustay_id
 
 -- patient level factors
 , pat.gender
-, adm.ethnicity, adm.ADMISSION_TYPE, adm.admittime, adm.dischtime, adm.hospital_expire_flag, adm.first_hosp_stay, adm.hospstay_seq
-  
+
+-- hospital level factors
+, adm.admittime, adm.dischtime
+, round((cast(adm.dischtime as date) - cast(adm.admittime as date)), 4) as LOS_HOSPITAL
+, round((cast(adm.admitime as date) - cast(pat.dob as date))  / 365.242), 4) as Age
+, adm.ethnicity, adm.ADMISSION_TYPE
+, adm.hospital_expire_flag
+
+, row_number() over (partition by adm.subject_id order by adm.admittime) as hospstay_seq
+, case 
+    when row_number() over (partition by adm.subject_id order by adm.admittime) = 1 then 'Y' 
+    else 'N' 
+  end as first_hosp_stay
+
 -- icu level factors
 , ie.intime, ie.outtime
-, round((EXTRACT(EPOCH FROM (ie.intime-pat.dob)) / 60 / 60 / 24 / 365.242) :: NUMERIC, 4) as Age
-, round((EXTRACT(EPOCH FROM (ie.outtime - ie.intime)) / 60 / 60 / 24) :: NUMERIC, 4) as LOS_ICU
-, row_number() over (partition by ie.subject_id, ie.hadm_id order by ie.intime) as icustay_seq
+, round( (cast(ie.outtime as date) - cast(ie.intime as date)) , 4) as LOS_ICU
+, row_number() over (partition by ie.hadm_id order by ie.intime) as icustay_seq
+-- first ICU stay *for the current hospitalization*
+, case 
+    when row_number() over (partition by ie.hadm_id order by ie.intime) = 1 then 'Y' 
+    else 'N' 
+  end as first_icu_stay
 
 from icustays ie
-inner join adm
+inner join admissions adm
  on ie.hadm_id = adm.hadm_id
 inner join patients pat
  on ie.subject_id = pat.subject_id
-)
-select co.*
-from co
-order by co.subject_id, co.admittime, co.intime;
+where ad.has_chartevents_data = 1
+order by ie.subject_id, ie.admittime, ie.intime;
