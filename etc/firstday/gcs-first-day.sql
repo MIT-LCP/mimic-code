fbaf1b586eeeef4331a9264da19c4c5853eaaa2f
Alistair Johnson <alistairewj@gmail.com>
2016-02-12 14:32:58 -0500

    changed logic - intub now sets GCS to 15

--- etc/firstday/gcs-first-day.sql
+++ etc/firstday/gcs-first-day.sql
@@ -13,7 +13,15 @@
 -- The code combines the ITEMIDs into the carevue itemids, then pivots those
 -- So 223900 is changed to 723, then the ITEMID 723 is pivoted to form GCSVerbal
 
-drop materialized view gcsfirstday;
+-- Note:
+--  The GCS for sedated patients is defaulted to 15 in this code.
+--  This is in line with how the data is meant to be collected.
+--  e.g., from the SAPS II publication:
+--    For sedated patients, the Glasgow Coma Score before sedation was used.
+--    This was ascertained either from interviewing the physician who ordered the sedation,
+--    or by reviewing the patient's medical record.
+
+drop materialized view gcsfirstday CASCADE;
 
 create materialized view gcsfirstday as
 with base as
@@ -78,23 +86,32 @@ with base as
 )
 , gcs as (
   select b.*
+  , b2.GCSVerbal as GCSVerbalPrev
+  , b2.GCSMotor as GCSMotorPrev
+  , b2.GCSEyes as GCSEyesPrev
   -- Calculate GCS, factoring in special case when they are intubated and prev vals
   -- note that the coalesce are used to implement the following if:
   --  if current value exists, use it
   --  if previous value exists, use it
   --  otherwise, default to normal
   , case
-      -- replace GCS of 3 due to sedation with 15
-      when b.GCSVerbal = 0 and b.GCSMotor = 1 and b.GCSEyes = 1
-        then 15
-      -- if intubated/trach'd, replace GCS verbal with 5
+      -- replace GCS during sedation with 15
       when b.GCSVerbal = 0
-        then coalesce(b.GCSMotor,coalesce(b2.GCSMotor,6)) + 5 + coalesce(b.GCSEyes,coalesce(b2.GCSEyes,4))
-      -- otherwise, add up score normally
-      when b.GCSVerbal is null and b2.GCSVerbal != 0
-        then coalesce(b.GCSMotor,coalesce(b2.GCSMotor,6)) + b2.GCSVerbal + coalesce(b.GCSEyes,coalesce(b2.GCSEyes,4))
-      else coalesce(b.GCSMotor,coalesce(b2.GCSMotor,6)) + b.GCSVerbal + coalesce(b.GCSEyes,coalesce(b2.GCSEyes,4)) end
-      as GCS
+        then 15
+      when b.GCSVerbal is null and b2.GCSVerbal = 0
+        then 15
+      -- if previously they were intub, but they aren't now, do not use previous GCS values
+      when b2.GCSVerbal = 0
+        then
+            coalesce(b.GCSMotor,6)
+          + coalesce(b.GCSVerbal,5)
+          + coalesce(b.GCSEyes,4)
+      -- otherwise, add up score normally, imputing previous value if none available at current time
+      else
+            coalesce(b.GCSMotor,coalesce(b2.GCSMotor,6))
+          + coalesce(b.GCSVerbal,coalesce(b2.GCSVerbal,5))
+          + coalesce(b.GCSEyes,coalesce(b2.GCSEyes,4))
+      end as GCS
 
   from base b
   -- join to itself within 6 hours to get previous value
@@ -113,9 +130,9 @@ with base as
 select ie.SUBJECT_ID, ie.HADM_ID, ie.ICUSTAY_ID
 -- The minimum GCS is determined by the above row partition, we only join if IsMinGCS=1
 , GCS as MinGCS
-, GCSMotor as MinGCSMotor
-, GCSVerbal as MinGCSVerbal
-, GCSEyes as MinGCSEyes
+, coalesce(GCSMotor,GCSMotorPrev) as GCSMotor
+, coalesce(GCSVerbal,GCSVerbalPrev) as GCSVerbal
+, coalesce(GCSEyes,GCSEyesPrev) as GCSEyes
 , EndoTrachFlag as EndoTrachFlag
 
 -- subselect down to the cohort of eligible patients
@@ -123,5 +140,3 @@ from mimiciii.icustays ie
 left join gcs_final gs
   on ie.ICUSTAY_ID = gs.ICUSTAY_ID and gs.IsMinGCS = 1
 ORDER BY ie.ICUSTAY_ID;
-
-commit;
