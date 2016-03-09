06fb0f35349b2fff3531a4ee62ba414df9d7ad87
Tom Pollard <tpollard@mit.edu>
2016-03-09 09:59:19 -0500

    add version.

--- cookbook/postgres/basic_patient_info.sql
+++ cookbook/postgres/basic_patient_info.sql
@@ -1,8 +1,8 @@
 -- ------------------------------------------------------------------
 -- Title: Retrieves basic patient information from the patients table 
--- MIMIC version: ?
+-- MIMIC version: MIMIC-III v1.3
 -- ------------------------------------------------------------------
 
 
-SELECT subject_id,gender, dob
+SELECT subject_id, gender, dob
 FROM mimiciii.patients;
