94d34c100d0ec548551a5a4b2cbed97c4176f660
Tom Pollard <tpollard@mit.edu>
2016-06-14 15:01:19 -0400

    Update icustay_detail.sql

--- demographics/postgres/icustay_detail.sql
+++ demographics/postgres/icustay_detail.sql
@@ -3,7 +3,7 @@
 -- Description: This query provides a useful set of information regarding patient
 --			ICU stays. The information is combined from the admissions, patients, and
 --			icustays tables. It includes age, length of stay, sequence, and expiry flags.
--- MIMIC version: MIMIC-III v1.4
+-- MIMIC version: MIMIC-III v1.3
 -- ------------------------------------------------------------------
 
 -- (Optional) Define which schema to work on
@@ -13,38 +13,36 @@
 DROP MATERIALIZED VIEW IF EXISTS icustay_detail;
 CREATE MATERIALIZED VIEW icustay_detail as
 
-select ie.subject_id, ie.hadm_id, ie.icustay_id
+SELECT ie.subject_id, ie.hadm_id, ie.icustay_id
 
 -- patient level factors
 , pat.gender
 
 -- hospital level factors
 , adm.admittime, adm.dischtime
-, round( (cast(adm.dischtime as date) - cast(adm.admittime as date)) , 4) as LOS_HOSPITAL
-, round( (cast(adm.admittime as date) - cast(pat.dob as date))  / 365.242, 4) as Age
+, ROUND( (CAST(adm.dischtime AS DATE) - CAST(adm.admittime AS DATE)) , 4) AS LOS_HOSPITAL
+, ROUND( (CAST(adm.admittime AS DATE) - CAST(pat.dob AS DATE))  / 365.242, 4) AS Age
 , adm.ethnicity, adm.ADMISSION_TYPE
 , adm.hospital_expire_flag
-
-, dense_rank() over (partition by adm.subject_id order by adm.admittime) as hospstay_seq
-, case
-    when dense_rank() over (partition by adm.subject_id order by adm.admittime) = 1 then 'Y'
-    else 'N'
-  end as first_hosp_stay
+, DENSE_RANK() OVER (PARTITION BY adm.subject_id ORDER BY adm.admittime) AS hospstay_seq
+, CASE
+    WHEN DENSE_RANK() OVER (PARTITION BY adm.subject_id ORDER BY adm.admittime) = 1 THEN 'Y'
+    ELSE 'N' END AS first_hosp_stay
 
 -- icu level factors
 , ie.intime, ie.outtime
-, round( (cast(ie.outtime as date) - cast(ie.intime as date)) , 4) as LOS_ICU
-, dense_rank() over (partition by ie.hadm_id order by ie.intime) as icustay_seq
--- first ICU stay *for the current hospitalization*
-, case
-    when dense_rank() over (partition by ie.hadm_id order by ie.intime) = 1 then 'Y'
-    else 'N'
-  end as first_icu_stay
+, ROUND( (CAST(ie.outtime AS DATE) - CAST(ie.intime AS DATE)) , 4) as LOS_ICU
+, DENSE_RANK() OVER (PARTITION BY ie.hadm_id ORDER BY ie.intime) as icustay_seq
 
-from icustays ie
-inner join admissions adm
- on ie.hadm_id = adm.hadm_id
-inner join patients pat
- on ie.subject_id = pat.subject_id
-where adm.has_chartevents_data = 1
-order by ie.subject_id, adm.admittime, ie.intime;
+-- first ICU stay *for the current hospitalization*
+, CASE
+    WHEN DENSE_RANK() OVER (PARTITION BY ie.hadm_id ORDER BY ie.intime) = 1 THEN 'Y'
+    ELSE 'N' END AS first_icu_stay
+
+FROM icustays ie
+INNER JOIN admissions adm
+    ON ie.hadm_id = adm.hadm_id
+INNER JOIN patients pat
+    ON ie.subject_id = pat.subject_id
+WHERE adm.has_chartevents_data = 1
+ORDER BY ie.subject_id, adm.admittime, ie.intime;
