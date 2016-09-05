c7b5cf127a691f748b49a61ca3833cee6253ab39
Alistair Johnson <alistairewj@gmail.com>
2016-09-05 12:57:25 -0400

    add 7zip import - useful for windows users

---
+++ buildmimic/postgres/postgres_load_data_7zip.sql
@@ -0,0 +1,183 @@
+-- -------------------------------------------------------------------------------
+--
+-- Load data into the MIMIC-III schema
+--
+-- -------------------------------------------------------------------------------
+
+--------------------------------------------------------
+--  File created - Thursday-August-27-2015
+--------------------------------------------------------
+
+\set ON_ERROR_STOP 1
+
+-- Change to the directory containing the data files
+\cd :mimic_data_dir
+
+-- Define the schema where all tables are created
+SET search_path TO mimiciii;
+
+-- Restoring the search path to its default value can be accomplished as follows:
+-- SET search_path TO "$user",public;
+
+/* Set the mimic_data_dir variable to point to directory containing
+   all .csv files. If using Docker, this should not be changed here.
+   Rather, when running the docker container, use the -v option
+   to have Docker mount a host volume to the container path /mimic_data
+   as explained in the README file
+*/
+
+--------------------------------------------------------
+--  Load Data for Table ADMISSIONS
+--------------------------------------------------------
+
+\copy ADMISSIONS FROM PROGRAM '7z e -so ADMISSIONS.csv.gz' DELIMITER ',' CSV HEADER NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table CALLOUT
+--------------------------------------------------------
+
+\copy CALLOUT from PROGRAM '7z e -so CALLOUT.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table CAREGIVERS
+--------------------------------------------------------
+
+\copy CAREGIVERS from PROGRAM '7z e -so CAREGIVERS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table CHARTEVENTS
+--------------------------------------------------------
+
+\copy CHARTEVENTS from PROGRAM '7z e -so CHARTEVENTS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table CPTEVENTS
+--------------------------------------------------------
+
+\copy CPTEVENTS from PROGRAM '7z e -so CPTEVENTS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table DATETIMEEVENTS
+--------------------------------------------------------
+
+\copy DATETIMEEVENTS from PROGRAM '7z e -so DATETIMEEVENTS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table DIAGNOSES_ICD
+--------------------------------------------------------
+
+\copy DIAGNOSES_ICD from PROGRAM '7z e -so DIAGNOSES_ICD.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table DRGCODES
+--------------------------------------------------------
+
+\copy DRGCODES from PROGRAM '7z e -so DRGCODES.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_CPT
+--------------------------------------------------------
+
+\copy D_CPT from PROGRAM '7z e -so D_CPT.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_ICD_DIAGNOSES
+--------------------------------------------------------
+
+\copy D_ICD_DIAGNOSES from PROGRAM '7z e -so D_ICD_DIAGNOSES.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_ICD_PROCEDURES
+--------------------------------------------------------
+
+\copy D_ICD_PROCEDURES from PROGRAM '7z e -so D_ICD_PROCEDURES.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_ITEMS
+--------------------------------------------------------
+
+\copy D_ITEMS from PROGRAM '7z e -so D_ITEMS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_LABITEMS
+--------------------------------------------------------
+
+\copy D_LABITEMS from PROGRAM '7z e -so D_LABITEMS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table ICUSTAYS
+--------------------------------------------------------
+
+\copy ICUSTAYS from PROGRAM '7z e -so ICUSTAYS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table INPUTEVENTS_CV
+--------------------------------------------------------
+
+\copy INPUTEVENTS_CV from PROGRAM '7z e -so INPUTEVENTS_CV.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table INPUTEVENTS_MV
+--------------------------------------------------------
+
+\copy INPUTEVENTS_MV from PROGRAM '7z e -so INPUTEVENTS_MV.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table LABEVENTS
+--------------------------------------------------------
+
+\copy LABEVENTS from PROGRAM '7z e -so LABEVENTS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table MICROBIOLOGYEVENTS
+--------------------------------------------------------
+
+\copy MICROBIOLOGYEVENTS from PROGRAM '7z e -so MICROBIOLOGYEVENTS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table NOTEEVENTS
+--------------------------------------------------------
+
+\copy NOTEEVENTS from PROGRAM '7z e -so NOTEEVENTS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table OUTPUTEVENTS
+--------------------------------------------------------
+
+\copy OUTPUTEVENTS from PROGRAM '7z e -so OUTPUTEVENTS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table PATIENTS
+--------------------------------------------------------
+
+\copy PATIENTS from PROGRAM '7z e -so PATIENTS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table PRESCRIPTIONS
+--------------------------------------------------------
+
+\copy PRESCRIPTIONS from PROGRAM '7z e -so PRESCRIPTIONS.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table PROCEDUREEVENTS_MV
+--------------------------------------------------------
+
+\copy PROCEDUREEVENTS_MV from PROGRAM '7z e -so PROCEDUREEVENTS_MV.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table PROCEDURES_ICD
+--------------------------------------------------------
+
+\copy PROCEDURES_ICD from PROGRAM '7z e -so PROCEDURES_ICD.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table SERVICES
+--------------------------------------------------------
+
+\copy SERVICES from PROGRAM '7z e -so SERVICES.csv.gz' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table TRANSFERS
+--------------------------------------------------------
+
+\copy TRANSFERS from PROGRAM '7z e -so TRANSFERS.csv.gz' delimiter ',' csv header NULL ''
