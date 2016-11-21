47b253ac3066855f188181bfae8dd4d2dced7a48
Tom Pollard <tpollard@mit.edu>
2016-11-21 11:32:50 -0500

    drop view. ref #153

--- etc/firstday/vitals-first-day.sql
+++ etc/firstday/vitals-first-day.sql
@@ -1,6 +1,7 @@
 -- This query pivots the vital signs for the first 24 hours of a patient's stay
 -- Vital signs include heart rate, blood pressure, respiration rate, and temperature
 
+DROP MATERIALIZED VIEW IF EXISTS vitalsfirstday CASCADE;
 create materialized view vitalsfirstday as
 SELECT pvt.subject_id, pvt.hadm_id, pvt.icustay_id
 
