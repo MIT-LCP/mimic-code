684175c398972154ea3893e88cf1ae07db886925
jueurousseau <35864508+jueurousseau@users.noreply.github.com>
2018-08-06 13:13:52 -0400

    Updated colloid bolus
    -- ITEM IDS IN fluid_input.sql which are NOT in our code -- for inputevents_mv_label
    
    -- FLUIDS IN:
    -- 220949;"Dextrose 5%";"ml" -- Colloids
    -- 220950;"Dextrose 10%";"ml" -- Colloids
    -- 220952;"Dextrose 50%";"ml" -- Colloids
    
    
    -- ITEM IDS IN fluid_input.sql which are NOT in our code -- for inputevents_cv_label
    
    -- FLUIDS IN:
    -- 30008;"Albumin 5%";"ml" -- Colloids
    -- 30016;"Dextrose 10%";"ml" -- Colloids
    -- 30187;"D10W";"ml" -- Colloids
    -- 30317;"D 12.5 W";"ml" -- Colloids
    -- 30318;"D15W";"ml" -- Colloids
    
    
    -- 30015 "D5/.45NS" -- mixed colloids and crystalloids

--- concepts/fluid-balance/colloid-bolus.sql
+++ concepts/fluid-balance/colloid-bolus.sql
@@ -28,6 +28,10 @@ with t1 as
     -- below ITEMIDs not in use
    -- 220861 | Albumin (Human) 20%
    -- 220863 | Albumin (Human) 4%
+   ,
+   220949, -- Dextrose 5%
+   220950, -- Dextrose 10%
+   220952 -- Dextrose 50%
   )
   and mv.statusdescription != 'Rewritten'
   and
@@ -72,6 +76,13 @@ with t1 as
   ,30011 -- Dextran 40
   ,30016 -- Dextrose 10%
   ,42731 -- Dextran40 10%
+  ,
+  30008, -- Albumin 5%
+  30016, -- Dextrose 10%
+  30187, -- D10W
+  30317, -- D 12.5 W
+  30318, --D15W
+  30015 -- D5/.45NS -- mixed colloids and crystalloids
   )
   and cv.amount > 100
   and cv.amount < 2000
