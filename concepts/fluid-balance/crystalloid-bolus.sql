810da7256226a082d7aaa5c6fa5916355f33fbb4
jueurousseau <35864508+jueurousseau@users.noreply.github.com>
2018-08-06 13:20:39 -0400

    Updated crystalloid bolus script
    -- ITEM IDS IN fluid_input.sql which are NOT in our code -- for inputevents_mv_label
    
    -- FLUIDS IN:
    -- 225159;"NaCl 0.45%";"ml" -- Crystalloids
    -- 225161;"NaCl 3% (Hypertonic Saline)";"ml" -- Crystalloids
    -- 225823;"D5 1/2NS";"ml" -- Crystalloids
    -- 225825;"D5NS";"ml" -- Crystalloids
    -- 225827;"D5LR";"ml" -- Crystalloids
    -- 225941;"D5 1/4NS";"ml" -- Crystalloids
    -- 226089;"Piggyback";"ml" -- Crystalloids
    
    -- ITEM IDS IN fluid_input.sql which are NOT in our code -- for inputevents_cv_label
    
    -- FLUIDS IN:
    -- 30020;".45% Normal Saline";"ml" -- Crystalloids
    -- 30060;"D5NS";"ml" -- Crystalloids
    -- 30061;"D5RL";"ml" -- Crystalloids
    -- 30143;"3% Normal Saline";"ml" -- Crystalloids
    -- 30159;"D5 Ringers Lact.";"ml" -- Crystalloids
    -- 30160;"D5 Normal Saline";"ml" -- Crystalloids
    -- 30190;"NS .9%";"ml" -- Crystalloids
    -- 30015 "D5/.45NS" -- mixed colloids and crystalloids

--- concepts/fluid-balance/crystalloid-bolus.sql
+++ concepts/fluid-balance/crystalloid-bolus.sql
@@ -21,6 +21,14 @@ with t1 as
     225828, -- LR
     225944, -- Sterile Water
     225797  -- Free Water
+    ,
+	  225159, -- NaCl 0.45%
+	  225161, -- NaCl 3% (Hypertonic Saline)
+	  225823, -- D5 1/2NS
+	  225825, -- D5NS
+	  225827, -- D5LR
+	  225941, -- D5 1/4NS
+	  226089 -- Piggyback
   )
   and mv.statusdescription != 'Rewritten'
   and
@@ -122,6 +130,15 @@ with t1 as
   , 44894 --	N/s 500 ml bolus
   , 41380 --	nsbolus
   , 42671 --	free h2o
+  ,
+  30020, -- .45% Normal Saline
+  30060, --D5NS
+  30061, -- D5RL
+  30143, -- 3% Normal Saline
+  30159, -- D5 Ringers Lact.
+  30160, -- D5 Normal Saline
+  30190, -- NS .9%
+  30015 -- "D5/.45NS" -- mixed colloids and crystalloids
   )
   and cv.amount > 248
   and cv.amount <= 2000
