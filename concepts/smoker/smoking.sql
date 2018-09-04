8cad6876dac16fb764aa0c9cd110fc27da527166
theonesp <ma.armengol.hcsc@gmail.com>
2018-09-04 15:36:09 -0400

    Update smoking.sql
    corrected code

--- concepts/smoker/smoking.sql
+++ concepts/smoker/smoking.sql
@@ -1,15 +1,16 @@
---Code to tell wether a person was a smoker         
-         WITH terms AS (
+--Code to tell whether a person was a smoker         
+WITH terms AS (
                  SELECT ne.subject_id,
                         CASE
-                            WHEN ne.text ~* '(never|not|not a|none|non|no|no history of|no h\/o of|denies|denies any|negative)[\s-]?(smoke|smoking|tabacco|tobacco|cigar|cigs)'::text OR ne.text ~* '(smoke|smoking|tabacco|tobacco|tabacco abuse|tobacco abuse|cigs|cigarettes):[\s]?(no|never|denies|negative)'::text OR ne.text ~* 'smoked no \[x\]|no etoh, tobacco|not drink alcohol or smoke|not drink or smoke|absence of current tobacco use|absence of tobacco use'::text THEN 0
-                            WHEN ne.text ~* '(smoke|smoking|tabacco|tobacco|cigar|cigs|marijuana|nicotine)'::text THEN 1
-                            ELSE NULL
+                            WHEN ne.text ~* '(never|not|not a|none|non|no|no history of|no h\/o of|denies|denies any|negative)[\s-]?(smoke|smoking|tabacco|tobacco|cigar|cigs)'::text OR ne.text ~* '(smoke|smoking|tabacco|tobacco|tabacco abuse|tobacco abuse|cigs|cigarettes):[\s]?(no|never|denies|negative)'::text OR ne.text ~* 'smoked no \[x\]|no etoh, tobacco|not drink alcohol or smoke|not drink or smoke|absence of current tobacco use|absence of tobacco use'::text THEN 0 --means no smoker
+                            WHEN ne.text ~* '(smoke|smoking|tabacco|tobacco|cigar|cigs|marijuana|nicotine)'::text THEN 1 --means smoker
+                            ELSE 2 --means unknown
                         END AS smoking
                    FROM mimiciii.noteevents ne
                 )
         SELECT terms.subject_id
-        ,min(terms.smoking) AS smoking
+        ,min(terms.smoking) AS smoking --chooses smoker,no smoker, unknown; in that order
         FROM terms
         GROUP BY subject_id
         ORDER BY subject_id
+
