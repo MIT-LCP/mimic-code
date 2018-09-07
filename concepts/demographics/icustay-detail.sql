6bb925654c6469355e3a0017513e7571712a7a60
Alistair Johnson <alistairewj@gmail.com>
2018-09-07 08:12:12 -0400

    add groups to ethnicity categories, rename age column

--- concepts/demographics/icustay-detail.sql
+++ concepts/demographics/icustay-detail.sql
@@ -21,8 +21,70 @@ SELECT ie.subject_id, ie.hadm_id, ie.icustay_id
 -- hospital level factors
 , adm.admittime, adm.dischtime
 , ROUND( (CAST(EXTRACT(epoch FROM adm.dischtime - adm.admittime)/(60*60*24) AS numeric)), 4) AS los_hospital
-, ROUND( (CAST(EXTRACT(epoch FROM adm.admittime - pat.dob)/(60*60*24*365.242) AS numeric)), 4) AS admission_age
-, adm.ethnicity, adm.admission_type
+, ROUND( (CAST(EXTRACT(epoch FROM adm.admittime - pat.dob)/(60*60*24*365.242) AS numeric)), 4) AS age
+, adm.ethnicity
+, case when ethnicity in
+  (
+       'WHITE' --  40996
+     , 'WHITE - RUSSIAN' --    164
+     , 'WHITE - OTHER EUROPEAN' --     81
+     , 'WHITE - BRAZILIAN' --     59
+     , 'WHITE - EASTERN EUROPEAN' --     25
+  ) then 'white'
+  when ethnicity in
+  (
+      'BLACK/AFRICAN AMERICAN' --   5440
+    , 'BLACK/CAPE VERDEAN' --    200
+    , 'BLACK/HAITIAN' --    101
+    , 'BLACK/AFRICAN' --     44
+    , 'CARIBBEAN ISLAND' --      9
+  ) then 'black'
+  when ethnicity in
+    (
+      'HISPANIC OR LATINO' --   1696
+    , 'HISPANIC/LATINO - PUERTO RICAN' --    232
+    , 'HISPANIC/LATINO - DOMINICAN' --     78
+    , 'HISPANIC/LATINO - GUATEMALAN' --     40
+    , 'HISPANIC/LATINO - CUBAN' --     24
+    , 'HISPANIC/LATINO - SALVADORAN' --     19
+    , 'HISPANIC/LATINO - CENTRAL AMERICAN (OTHER)' --     13
+    , 'HISPANIC/LATINO - MEXICAN' --     13
+    , 'HISPANIC/LATINO - COLOMBIAN' --      9
+    , 'HISPANIC/LATINO - HONDURAN' --      4
+  ) then 'hispanic'
+  when ethnicity in
+  (
+      'ASIAN' --   1509
+    , 'ASIAN - CHINESE' --    277
+    , 'ASIAN - ASIAN INDIAN' --     85
+    , 'ASIAN - VIETNAMESE' --     53
+    , 'ASIAN - FILIPINO' --     25
+    , 'ASIAN - CAMBODIAN' --     17
+    , 'ASIAN - OTHER' --     17
+    , 'ASIAN - KOREAN' --     13
+    , 'ASIAN - JAPANESE' --      7
+    , 'ASIAN - THAI' --      4
+  ) then 'asian'
+  when ethnicity in
+  (
+       'AMERICAN INDIAN/ALASKA NATIVE' --     51
+     , 'AMERICAN INDIAN/ALASKA NATIVE FEDERALLY RECOGNIZED TRIBE' --      3
+  ) then 'native'
+  when ethnicity in
+  (
+      'UNKNOWN/NOT SPECIFIED' --   4523
+    , 'UNABLE TO OBTAIN' --    814
+    , 'PATIENT DECLINED TO ANSWER' --    559
+  ) then 'unknown'
+  else 'other' end as ethnicity
+  -- , 'OTHER' --   1512
+  -- , 'MULTI RACE ETHNICITY' --    130
+  -- , 'PORTUGUESE' --     61
+  -- , 'MIDDLE EASTERN' --     43
+  -- , 'NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER' --     18
+  -- , 'SOUTH AMERICAN' --      8
+
+, adm.ADMISSION_TYPE
 , adm.hospital_expire_flag
 , DENSE_RANK() OVER (PARTITION BY adm.subject_id ORDER BY adm.admittime) AS hospstay_seq
 , CASE
