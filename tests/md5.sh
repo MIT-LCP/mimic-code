727db49af82337a3fde366209386193102902b13
Matt Sabs <matt.sabs@gmail.com>
2018-12-31 12:19:49 -0500

    -z -> -f

--- tests/md5.sh
+++ tests/md5.sh
@@ -1,13 +1,13 @@
 while read p; do
   A="$(echo $p | cut -d' ' -f1)"
   B="$(echo $p | cut -d' ' -f2)"
-  read C< <(md5 -q $B)
-  echo $B $A = $C
-  if [ ! -z $B ]
+  if [ ! -f $B ]
   then
     echo "ERROR: $B does not exist"
-    exit
+    continue
   fi
+  read C< <(md5 -q $B)
+  echo $B $A = $C
   if [ $C != $A ]
   then
     echo "ERROR: $B wrong md5"
