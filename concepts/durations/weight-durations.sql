81dee333898398d73f3e77130371fd6cd7e6110b
Alistair Johnson <alistairewj@gmail.com>
2019-07-22 14:52:43 -0400

    bring over height/weight fixes from master

--- concepts/durations/weight-durations.sql
+++ concepts/durations/weight-durations.sql
@@ -14,7 +14,7 @@ WITH wt_neonate AS
     FROM `physionet-data.mimiciii_clinical.chartevents` c
     WHERE c.itemid in (3580, 3581, 3582)
     AND c.icustay_id IS NOT NULL
-    AND c.error IS DISTINCT FROM 1
+    AND COALESCE(c.error, 0) = 0
     -- wt_oz/wt_lb/wt_kg are only 0 erroneously, so drop these rows
     AND c.valuenum > 0
   -- a separate query was run to manually verify only 1 value exists per
@@ -31,7 +31,7 @@ WITH wt_neonate AS
         -- clean free-text birth weight data
         CASE
           -- ignore value if there are any non-numeric characters
-          WHEN c.value ~ '[^0-9\.]' THEN NULL 
+          WHEN REGEXP_CONTAINS(c.value, r'[^0-9\.]') THEN NULL 
           -- convert grams to kd
           WHEN CAST(c.value AS NUMERIC) > 100 THEN CAST(c.value AS NUMERIC)/1000
           -- keep kg as is, filtering bad values (largest baby ever born was conveniently 9.98kg)
@@ -44,7 +44,7 @@ WITH wt_neonate AS
     FROM `physionet-data.mimiciii_clinical.chartevents` c
     WHERE c.itemid in (3723, 4183)
     AND c.icustay_id IS NOT NULL
-    AND c.error IS DISTINCT FROM 1
+    AND COALESCE(c.error, 0) = 0
   -- a separate query was run to manually verify only 1 value exists per
   -- icustay_id/charttime/itemid grouping
   -- therefore, we can use max() across itemid to collapse these values to 1 row per group
@@ -69,7 +69,7 @@ WITH wt_neonate AS
       AND c.icustay_id IS NOT NULL
       AND c.valuenum > 0
       -- exclude rows marked as error
-      AND (c.error IS NULL OR c.error = 0)
+      AND COALESCE(c.error, 0) = 0
     UNION ALL
     SELECT
         n.icustay_id
@@ -111,8 +111,8 @@ WITH wt_neonate AS
         then DATETIME_SUB(ie.intime, INTERVAL 2 HOUR)
       else wt_stg1.charttime end as starttime
     , wt_stg1.weight
-  FROM `physionet-data.mimiciii_clinical.icustays` ie
-  inner join wt_stg1
+  from wt_stg1
+  INNER JOIN `physionet-data.mimiciii_clinical.icustays` ie
     on ie.icustay_id = wt_stg1.icustay_id
 )
 , wt_stg3 as
@@ -132,19 +132,16 @@ WITH wt_neonate AS
 , wt1 as
 (
   select
-      ie.icustay_id
-    , wt.starttime
-    , case when wt.icustay_id is null then null
-      else
-        coalesce(wt.endtime,
-        LEAD(wt.starttime) OVER (partition by ie.icustay_id order by wt.starttime),
-          -- we add a 2 hour "fuzziness" window
-        DATETIME_ADD(ie.outtime, INTERVAL 2 HOUR))
-      end as endtime
-    , wt.weight
-  FROM `physionet-data.mimiciii_clinical.icustays` ie
-  left join wt_stg3 wt
-    on ie.icustay_id = wt.icustay_id
+      icustay_id
+    , starttime
+    , coalesce(endtime,
+      LEAD(starttime) OVER (partition by icustay_id order by starttime),
+      -- impute ICU discharge as the end of the final weight measurement
+      -- plus a 2 hour "fuzziness" window
+      DATETIME_ADD(outtime, INTERVAL 2 HOUR)
+    ) as endtime
+    , weight
+  from wt_stg3
 )
 -- if the intime for the patient is < the first charted daily weight
 -- then we will have a "gap" at the start of their stay
@@ -157,7 +154,7 @@ WITH wt_neonate AS
     , DATETIME_SUB(ie.intime, INTERVAL 2 HOUR) as starttime
     , wt.starttime as endtime
     , wt.weight
-  FROM `physionet-data.mimiciii_clinical.icustays` ie
+  from `physionet-data.mimiciii_clinical.icustays` ie
   inner join
   -- the below subquery returns one row for each unique icustay_id
   -- the row contains: the first starttime and the corresponding weight
@@ -201,17 +198,17 @@ WITH wt_neonate AS
     , LEAD(ec.charttime) OVER (PARTITION BY ie.icustay_id ORDER BY ec.charttime) as endtime
   from `physionet-data.mimiciii_clinical.icustays` ie
   inner join `physionet-data.mimiciii_notes.echodata` ec
-      on ie.hadm_id = ec.hadm_id
+    on ie.hadm_id = ec.hadm_id
   where ec.weight is not null
 )
 , echo_final as
 (
     select
-        el.icustay_id
-        , el.starttime
-          -- we add a 2 hour "fuzziness" window
-        , coalesce(el.endtime, DATETIME_ADD(el.outtime, INTERVAL 2 HOUR)) as endtime
-        , weight_echo
+      el.icustay_id
+      , el.starttime
+        -- we add a 2 hour "fuzziness" window
+      , coalesce(el.endtime, DATETIME_ADD(el.outtime, INTERVAL 2 HOUR)) as endtime
+      , weight_echo
     from echo_lag el
     UNION ALL
     -- if the starttime was later than ICU admission, back-propogate the weight
