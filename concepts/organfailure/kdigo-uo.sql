d9559ea61b8d7f4fe1253418d9e5c432be3b115d
Alistair Johnson <alistairewj@gmail.com>
2019-07-25 20:00:24 -0400

    calculate rate based off the interval used to sum UO, rather than assuming it's 6/12/24 hours

--- concepts/organfailure/kdigo-uo.sql
+++ concepts/organfailure/kdigo-uo.sql
@@ -3,27 +3,48 @@ CREATE MATERIALIZED VIEW kdigo_uo AS
 with ur_stg as
 (
   select io.icustay_id, io.charttime
-  -- three sums:
+  -- we have joined each row to all rows preceding within 24 hours
+  -- we can now sum these rows to get total UO over the last 24 hours
+  -- we can use case statements to restrict it to only the last 6/12 hours
+  -- therefore we have three sums:
   -- 1) over a 6 hour period
   -- 2) over a 12 hour period
   -- 3) over a 24 hour period
+  -- note that we assume data charted at charttime corresponds to 1 hour of UO
+  -- therefore we use '5' and '11' to restrict the period, rather than 6/12
+  -- this assumption may overestimate UO rate when documentation is done less than hourly
+
+  -- 6 hours
   , sum(case when io.charttime <= iosum.charttime + interval '5' hour
       then iosum.VALUE
     else null end) as UrineOutput_6hr
+  -- 12 hours
   , sum(case when io.charttime <= iosum.charttime + interval '11' hour
       then iosum.VALUE
     else null end) as UrineOutput_12hr
+  -- 24 hours
   , sum(iosum.VALUE) as UrineOutput_24hr
-  -- count number of measures to protect against missing data
-  , MIN(case when io.charttime <= iosum.charttime + interval '5' hour
-      then iosum.charttime
-    else null end) AS uo_tm_6hr
-  , MIN(case when io.charttime <= iosum.charttime + interval '11' hour
-      then iosum.charttime
-    else null end) AS uo_tm_12hr
-  , MIN(iosum.charttime) AS uo_tm_24hr
+  -- calculate the number of hours over which we've tabulated UO
+  , ROUND(EXTRACT(EPOCH FROM
+      io.charttime - 
+        -- below MIN() gets the earliest time that was used in the summation 
+        MIN(case when io.charttime <= iosum.charttime + interval '5' hour
+          then iosum.charttime
+        else null end)
+    -- convert from EPOCH (seconds) to hours by dividing by 360.0
+    )/360.0, 4) AS uo_tm_6hr
+  -- repeat extraction for 12 hours and 24 hours
+  , ROUND(EXTRACT(EPOCH FROM
+      io.charttime - 
+        MIN(case when io.charttime <= iosum.charttime + interval '11' hour
+          then iosum.charttime
+        else null end)
+   )/360.0, 4) AS uo_tm_12hr
+  , ROUND(EXTRACT(EPOCH FROM
+      io.charttime - MIN(iosum.charttime)
+   )/360.0, 4) AS uo_tm_24hr
   from urineoutput io
-  -- this join gives you all UO measurements over a 24 hour period
+  -- this join gives all UO measurements over the 24 hours preceding this row
   left join urineoutput iosum
     on  io.icustay_id = iosum.icustay_id
     and io.charttime >= iosum.charttime
@@ -37,10 +58,10 @@ select
 , ur.UrineOutput_6hr
 , ur.UrineOutput_12hr
 , ur.UrineOutput_24hr
--- calculate rates
-, ROUND((ur.UrineOutput_6hr/wd.weight/6.0)::NUMERIC, 4) AS uo_rt_6hr
-, ROUND((ur.UrineOutput_12hr/wd.weight/12.0)::NUMERIC, 4) AS uo_rt_12hr
-, ROUND((ur.UrineOutput_24hr/wd.weight/24.0)::NUMERIC, 4) AS uo_rt_24hr
+-- calculate rates - adding 1 hour as we assume data charted at 10:00 corresponds to previous hour
+, ROUND((ur.UrineOutput_6hr/wd.weight/(uo_tm_6hr+1))::NUMERIC, 4) AS uo_rt_6hr
+, ROUND((ur.UrineOutput_12hr/wd.weight/(uo_tm_12hr+1))::NUMERIC, 4) AS uo_rt_12hr
+, ROUND((ur.UrineOutput_24hr/wd.weight/(uo_tm_24hr+1))::NUMERIC, 4) AS uo_rt_24hr
 -- time of earliest UO measurement that was used to calculate the rate
 , uo_tm_6hr
 , uo_tm_12hr
