8032168df3deed5a9a04a41fab19ed70a8fac763
themsabs <msabatin@stevens.edu>
2019-08-16 17:30:52 -0400

    md5 txt checker for freeBSD (no -c option) (#513)

---
+++ tests/md5.sh
@@ -0,0 +1,16 @@
+while read p; do
+  A="$(echo $p | cut -d' ' -f1)"
+  B="$(echo $p | cut -d' ' -f2)"
+  if [ ! -f $B ]
+  then
+    echo "ERROR: $B does not exist"
+    continue
+  fi
+  read C< <(md5 -q $B)
+  echo $B $A = $C
+  if [ $C != $A ]
+  then
+    echo "ERROR: $B wrong md5"
+    exit
+  fi
+done <$1
