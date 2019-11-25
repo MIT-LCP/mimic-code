b36c78f338971303d0a0fa8238480ace51d4fb19
Alistair Johnson <alistairewj@gmail.com>
2019-11-25 13:55:37 -0500

    fix: ccs data loaded incorrectly, so refactor into 1 table

--- concepts/diagnosis/ccs_diagnosis_table.sql
+++ concepts/diagnosis/ccs_diagnosis_table.sql
@@ -1,22 +1,39 @@
-DROP TABLE IF EXISTS ccs_single_level_dx;
-CREATE TABLE ccs_single_level_dx
+DROP TABLE IF EXISTS ccs_dx;
+CREATE TABLE ccs_dx
 (
-  ccs_id INT NOT NULL,
-  ccs_name VARCHAR(150) NOT NULL,
-  icd9_code CHAR(5) NOT NULL
-);
-
-\copy ccs_single_level_dx from program 'gzip -dc ccs_single_level.csv.gz' CSV HEADER;
-
-DROP TABLE IF EXISTS ccs_multi_level_dx;
-CREATE TABLE ccs_multi_level_dx
-(
-  ccs_mid VARCHAR(10) NOT NULL,
-  ccs_name VARCHAR(100) NOT NULL,
+  icd9_code CHAR(5) NOT NULL,
+  -- we will populate ccs_mid and ccs_name with the most granular ID/name later
+  ccs_mid VARCHAR(10),
+  ccs_name VARCHAR(100),
+  ccs_id INTEGER,
+  -- CCS levels and names based on position in hierarchy
+  ccs_level1 VARCHAR(10),
   ccs_group1 VARCHAR(100),
+  ccs_level2 VARCHAR(10),
   ccs_group2 VARCHAR(100),
+  ccs_level3 VARCHAR(10),
   ccs_group3 VARCHAR(100),
-  icd9_code CHAR(5) NOT NULL
+  ccs_level4 VARCHAR(10),
+  ccs_group4 VARCHAR(100)
 );
 
-\copy ccs_multi_level_dx from program 'gzip -dc ccs_multi_level.csv.gz' CSV HEADER;
+-- copy all columns *but* ccs_mid and ccs_name
+-- we will populate these after the data is loaded in
+\COPY ccs_dx (icd9_code, ccs_level1, ccs_group1, ccs_level2, ccs_group2, ccs_level3, ccs_group3, ccs_level4, ccs_group4) FROM PROGRAM 'gzip -dc ccs_multi_dx.csv.gz' CSV HEADER;
+
+-- add in matched ID, name, and ccs_id
+--  matched id (ccs_mid): the ccs ID with the hierachy, e.g. 7.1.2.1
+--  name (ccs_name): the most granular CCS category the diagnosis is in
+--  ID (ccs_id): the CCS identifier for the ICD-9 code (integer)
+UPDATE ccs_dx
+SET ccs_mid=tt.ccs_mid, ccs_name=tt.ccs_name, ccs_id=tt.ccs_id
+FROM (
+  SELECT icd9_code
+  -- remove the trailing ccs_id from name column, i.e. "Burns [240.]" -> "Burns"
+  , REGEXP_REPLACE(COALESCE(ccs_group4, ccs_group3, ccs_group2, ccs_group1), '\[[0-9]+\.\]$', '') as ccs_name
+  -- extract the trailing ccs_id from name, i.e. "Burns [240.]" -> "240"
+  , SUBSTRING(COALESCE(ccs_group4, ccs_group3, ccs_group2, ccs_group1), '\[([0-9]+)\.\]$') as ccs_id
+  , COALESCE(ccs_level4, ccs_level3, ccs_level2, ccs_level1) AS ccs_mid
+  FROM ccs_dx
+) AS tt
+WHERE ccs_dx.icd9_code = tt.icd9_code;
