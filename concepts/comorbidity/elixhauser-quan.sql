f2283007ec894b4ae0aaef8be3856badf7fc30f4
Alistair Johnson <alistairewj@gmail.com>
2020-06-07 14:47:33 -0400

    refactor to remove an inline view

--- concepts/comorbidity/elixhauser-quan.sql
+++ concepts/comorbidity/elixhauser-quan.sql
@@ -15,13 +15,7 @@
 --  3) The first 3 characters of ICD9_CODE are compared to 3 character codes
 
 CREATE TABLE `physionet-data.mimiciii_derived.elixhauser_quan` AS
-with icd as
-(
-  select hadm_id, seq_num, icd9_code
-  from `physionet-data.mimiciii_clinical.diagnoses_icd`
-  where seq_num != 1 -- we do not include the primary icd-9 code
-)
-, eliflg as
+with eliflg as
 (
 select hadm_id, seq_num, icd9_code
 , CASE
@@ -182,7 +176,8 @@ select hadm_id, seq_num, icd9_code
   when SUBSTR(icd9_code, 1, 4) in ('2962','2963','2965','3004') then 1
   when SUBSTR(icd9_code, 1, 3) in ('309','311') then 1
   else 0 end as DEPRESS  /* Depression */
-from icd
+from `physionet-data.mimiciii_clinical.diagnoses_icd` icd
+where seq_num != 1 -- we do not include the primary icd-9 code
 )
 -- collapse the icd9_code specific flags into hadm_id specific flags
 -- this groups comorbidities together for a single patient admission
