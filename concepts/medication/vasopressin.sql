57533a99f963db7b50344605cd6a74d5c68f66b5
Alistair Johnson <alistairewj@gmail.com>
2020-09-08 12:00:43 -0400

    remove extra inline view and obsolete statusdescription where

--- concepts/medication/vasopressin.sql
+++ concepts/medication/vasopressin.sql
@@ -1,17 +1,9 @@
 -- This query extracts dose+durations of vasopressin administration
-with vasomv as
-(
-  select
-    stay_id, linkorderid
-    , rate as vaso_rate
-    , amount as vaso_amount
-    , starttime
-    , endtime
-  from `physionet-data.mimic_icu.inputevents`
-  where itemid = 222315 -- vasopressin
-  and statusdescription != 'Rewritten' -- only valid orders
-)
-SELECT stay_id
-  , starttime, endtime
-  , vaso_rate, vaso_amount
-from vasomv;
+select
+  stay_id, linkorderid
+  , rate as vaso_rate
+  , amount as vaso_amount
+  , starttime
+  , endtime
+from `physionet-data.mimic_icu.inputevents`
+where itemid = 222315 -- vasopressin
