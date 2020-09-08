95c99255e7093458d504cb90a1e02eed577dff3a
Alistair Johnson <alistairewj@gmail.com>
2020-09-07 20:10:33 -0400

    fix: syntax errors in grouping

--- concepts/firstday/first_day_weight.sql
+++ concepts/firstday/first_day_weight.sql
@@ -3,7 +3,8 @@
 -- sometimes used to monitor fluid balance.
 -- The MIMIC-III version used echodata but this isn't available in MIMIC-IV.
 SELECT
-  ie.stay_id
+  ie.subject_id
+  , ie.stay_id
   , AVG(CASE WHEN weight_type = 'admit' THEN weight ELSE NULL END) AS weight_admit
   , AVG(weight) AS weight
   , MIN(weight) AS weight_min
@@ -14,4 +15,5 @@ LEFT JOIN `physionet-data.mimic_derived.weight_durations`
     ON ie.stay_id = ce.stay_id
     -- we filter to weights documented during or before the 1st day
     AND ce.starttime <= DATETIME_ADD(ie.intime, INTERVAL '1' DAY)
+GROUP BY ie.subject_id, ie.stay_id
 ;
