34ff4867c28561fd7a5f079f5a87533a3f035074
Alistair Johnson <alistairewj@gmail.com>
2020-11-27 12:39:38 -0500

    add shell script to build tables on GCP

---
+++ concepts/make_concepts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This script generates the concepts in the BigQuery table mimic_derived.
+export TARGET_DATASET=mimic_derived
+
+# generate tables in subfolders
+for d in demographics measurement medication treatment firstday;
+do
+    for fn in `ls $d`;
+    do
+        # only run SQL queries
+        if [[ "${fn: -4}" == ".sql" ]]; then
+            # table name is file name minus extension
+            tbl="${fn::-4}"
+
+            echo "Generating ${TARGET_DATASET}.${tbl}"
+            bq query --use_legacy_sql=False --replace --destination_table=${TARGET_DATASET}.${tbl} < ${d}/${fn}
+        fi
+    done
+done
+
