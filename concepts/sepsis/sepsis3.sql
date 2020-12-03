cbd1b19145c9dd2272ce660c96ccf8dab6cbf454
Alistair Johnson <alistairewj@gmail.com>
2020-12-03 17:06:25 -0500

    extract sofa>=2 prior to join with sus

--- concepts/sepsis/sepsis3.sql
+++ concepts/sepsis/sepsis3.sql
@@ -2,7 +2,24 @@
 -- That is, the earliest time at which a patient had SOFA >= 2 and suspicion of infection.
 -- As many variables used in SOFA are only collected in the ICU, this query can only
 -- define sepsis-3 onset within the ICU.
-WITH s1 as
+
+-- extract rows with SOFA >= 2
+-- implicitly this assumes baseline SOFA was 0 before ICU admission.
+WITH sofa AS
+(
+  SELECT stay_id
+    , starttime, endtime
+    , respiration_24hours as respiration
+    , coagulation_24hours as coagulation
+    , liver_24hours as liver
+    , cardiovascular_24hours as cardiovascular
+    , cns_24hours as cns
+    , renal_24hours as renal
+    , sofa_24hours as sofa_score
+  FROM `physionet-data.mimic_derived.sofa`
+  WHERE sofa_24hours >= 2
+)
+, s1 as
 (
   SELECT 
     soi.subject_id
@@ -18,30 +35,14 @@ WITH s1 as
     , soi.positive_culture
     -- sofa columns
     , starttime, endtime
-    , respiration_24hours as respiration
-    , coagulation_24hours as coagulation
-    , liver_24hours as liver
-    , cardiovascular_24hours as cardiovascular
-    , cns_24hours as cns
-    , renal_24hours as renal
-    , coalesce(respiration_24hours, 0)
-      + coalesce(coagulation_24hours, 0)
-      + coalesce(liver_24hours, 0)
-      + coalesce(cardiovascular_24hours, 0)
-      + coalesce(cns_24hours, 0)
-      + coalesce(renal_24hours, 0) as sofa_score
+    , respiration, coagulation, liver, cardiovascular, cns, renal
+    , sofa_score
     -- All rows have an associated suspicion of infection event
     -- Therefore, Sepsis-3 is defined as SOFA >= 2.
     -- Implicitly, the baseline SOFA score is assumed to be zero, as we do not know
     -- if the patient has preexisting (acute or chronic) organ dysfunction 
     -- before the onset of infection.
-    , coalesce(respiration_24hours, 0)
-      + coalesce(coagulation_24hours, 0)
-      + coalesce(liver_24hours, 0)
-      + coalesce(cardiovascular_24hours, 0)
-      + coalesce(cns_24hours, 0)
-      + coalesce(renal_24hours, 0) >= 2 
-      and suspected_infection = 1 as sepsis3
+    , sofa_score >= 2 and suspected_infection = 1 as sepsis3
     -- subselect to the earliest suspicion/antibiotic/SOFA row
     , ROW_NUMBER() OVER
     (
@@ -49,7 +50,7 @@ WITH s1 as
         ORDER BY suspected_infection_time, antibiotic_time, culture_time, endtime
     ) AS rn_sus
   FROM `physionet-data.mimic_derived.suspicion_of_infection` as soi
-  INNER JOIN `physionet-data.mimic_derived.sofa` as sofa
+  INNER JOIN sofa
     ON soi.stay_id = sofa.stay_id 
     AND sofa.endtime >= DATETIME_SUB(soi.suspected_infection_time, INTERVAL 48 HOUR)
     AND sofa.endtime <= DATETIME_ADD(soi.suspected_infection_time, INTERVAL 24 HOUR)
