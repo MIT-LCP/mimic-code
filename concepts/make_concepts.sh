282a836ab43a81f808992147e94fd4e7c131db1e
Alistair Johnson <alistairewj@gmail.com>
2020-12-12 16:41:26 -0500

    move first_day_sofa to last table generated

--- concepts/make_concepts.sh
+++ concepts/make_concepts.sh
@@ -12,9 +12,16 @@ do
             # table name is file name minus extension
             tbl="${fn::-4}"
 
+            # skip first_day_sofa as it depends on other firstday queries
+            if [[ "${tbl}" == "first_day_sofa" ]]; then
+                continue
+            fi
             echo "Generating ${TARGET_DATASET}.${tbl}"
             bq query --use_legacy_sql=False --replace --destination_table=${TARGET_DATASET}.${tbl} < ${d}/${fn}
         fi
     done
 done
 
+# generate first_day_sofa table last
+echo "Generating ${TARGET_DATASET}.first_day_sofa"
+bq query --use_legacy_sql=False --replace --destination_table=${TARGET_DATASET}.first_day_sofa < firstday/first_day_sofa.sql
