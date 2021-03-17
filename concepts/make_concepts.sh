f07b281bdc1808cdba20dea19876c04e3c1228d9
Alistair Johnson <alistairewj@gmail.com>
2021-03-17 19:04:40 -0400

    add comorbidity/organfailure/sepsis subfolders

--- concepts/make_concepts.sh
+++ concepts/make_concepts.sh
@@ -3,7 +3,11 @@
 export TARGET_DATASET=mimic_derived
 
 # generate tables in subfolders
-for d in demographics measurement medication treatment firstday score;
+# order is important for a few tables here:
+# * firstday should go last
+# * sepsis depends on score (sofa.sql in particular)
+# * organfailure depends on measurement
+for d in comorbidity demographics measurement medication organfailure treatment score sepsis firstday;
 do
     for fn in `ls $d`;
     do
@@ -15,6 +19,9 @@ do
             # skip first_day_sofa as it depends on other firstday queries
             if [[ "${tbl}" == "first_day_sofa" ]]; then
                 continue
+            # kdigo_stages needs to be run after creat/uo
+            elif [[ "${tbl}" == "kdigo_stages" ]]; then
+                continue
             fi
             echo "Generating ${TARGET_DATASET}.${tbl}"
             bq query --use_legacy_sql=False --replace --destination_table=${TARGET_DATASET}.${tbl} < ${d}/${fn}
@@ -25,3 +32,7 @@ done
 # generate first_day_sofa table last
 echo "Generating ${TARGET_DATASET}.first_day_sofa"
 bq query --use_legacy_sql=False --replace --destination_table=${TARGET_DATASET}.first_day_sofa < firstday/first_day_sofa.sql
+
+# generate first_day_sofa table last
+echo "Generating ${TARGET_DATASET}.kdigo_stages"
+bq query --use_legacy_sql=False --replace --destination_table=${TARGET_DATASET}.kdigo_stages < organfailure/kdigo_stages.sql
