4b0ed10156df35af5694bcbfdf13a3c56855ff24
Angus Zhang <anguszhang1992@gmail.com>
2021-03-27 18:28:39 +0800

    Update kdigo_creatinine.sql
    fix `DATETIME_DIFF` error

--- concepts/organfailure/kdigo_creatinine.sql
+++ concepts/organfailure/kdigo_creatinine.sql
@@ -11,7 +11,7 @@ select
     on ie.subject_id = le.subject_id
     and le.ITEMID = 50912
     and le.VALUENUM is not null
-    and DATETIME_DIFF(le.charttime, ie.intime, HOUR) <= (7*24-6)
+    and DATETIME_DIFF(le.charttime, ie.intime, 'HOUR') <= (7*24-6)
     and le.CHARTTIME >= DATETIME_SUB(ie.intime, INTERVAL '6' HOUR)
     and le.CHARTTIME <= DATETIME_ADD(ie.intime, INTERVAL '7' DAY)
 )
@@ -27,11 +27,11 @@ FROM cr
 LEFT JOIN cr cr48
   ON cr.icustay_id = cr48.icustay_id
   AND cr48.charttime <  cr.charttime
-  AND DATETIME_DIFF(cr.charttime, cr48.charttime, HOUR) <= 48
+  AND DATETIME_DIFF(cr.charttime, cr48.charttime, 'HOUR') <= 48
 -- add in all creatinine values in the last 7 days
 LEFT JOIN cr cr7
   ON cr.icustay_id = cr7.icustay_id
   AND cr7.charttime <  cr.charttime
-  AND DATETIME_DIFF(cr.charttime, cr7.charttime, DAY) <= 7
+  AND DATETIME_DIFF(cr.charttime, cr7.charttime, 'DAY') <= 7
 GROUP BY cr.icustay_id, cr.charttime, cr.creat
 ORDER BY cr.icustay_id, cr.charttime, cr.creat;
