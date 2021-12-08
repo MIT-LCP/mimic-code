4c8e368a065da215b87109deba741e60c95a5599
Alistair Johnson <alistairewj@gmail.com>
2021-12-08 13:27:24 -0500

    validate/harmonize units of measure for vasopressors

--- mimic-iv/concepts/medication/phenylephrine.sql
+++ mimic-iv/concepts/medication/phenylephrine.sql
@@ -1,7 +1,9 @@
 -- This query extracts dose+durations of phenylephrine administration
 select
   stay_id, linkorderid
-  , rate as vaso_rate
+  -- one row in mcg/min, the rest in mcg/kg/min
+  , CASE WHEN rateuom = 'mcg/min' THEN rate / patientweight
+  ELSE rate END as vaso_rate
   , amount as vaso_amount
   , starttime
   , endtime
