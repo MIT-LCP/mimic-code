8292ad7b1bcaf498d024aba69517c9c7f987c4f1
Alistair Johnson <alistairewj@gmail.com>
2021-12-08 17:11:57 -0500

    round to 4 decimal places, fix null values forcing result to be null

--- mimic-iv/concepts/medication/norepinephrine_equivalent_dose.sql
+++ mimic-iv/concepts/medication/norepinephrine_equivalent_dose.sql
@@ -3,12 +3,15 @@
 -- by Goradia et al. 2020.
 SELECT stay_id, starttime, endtime
 -- calculate the dose
-, norepinephrine
-  + epinephrine
-  + phenylephrine/10
-  + dopamine/100
+, ROUND(COALESCE(norepinephrine, 0)
+  + COALESCE(epinephrine, 0)
+  + COALESCE(phenylephrine/10, 0)
+  + COALESCE(dopamine/100, 0)
   -- + metaraminol/8 -- metaraminol not used in BIDMC
-  + vasopressin*2.5
+  + COALESCE(vasopressin*2.5, 0)
+  -- angotensin_ii*10 -- angitensin ii rarely used, currently not incorporated
+  -- (it could be included due to norepinephrine sparing effects)
+  , 4) AS norepinephrine_equivalent_dose
   -- angotensin_ii*10 -- angitensin ii rarely used, currently not incorporated
   -- (it could be included due to norepinephrine sparing effects)
   AS norepinephrine_equivalent_dose
