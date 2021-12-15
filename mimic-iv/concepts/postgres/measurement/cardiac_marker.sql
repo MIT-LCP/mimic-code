a838ffa9d2a251cc3da2d1523a73fcee850195dc
Alistair Johnson <alistairewj@gmail.com>
2021-12-14 22:21:12 -0500

    add ntprobnp to postgres cardiac marker concept

--- mimic-iv/concepts/postgres/measurement/cardiac_marker.sql
+++ mimic-iv/concepts/postgres/measurement/cardiac_marker.sql
@@ -7,16 +7,18 @@ SELECT
   , MAX(charttime) AS charttime
   , le.specimen_id
   -- convert from itemid into a meaningful column
-  , MAX(CASE WHEN itemid = 51002 THEN value ELSE NULL END) AS troponin_i
   , MAX(CASE WHEN itemid = 51003 THEN value ELSE NULL END) AS troponin_t
   , MAX(CASE WHEN itemid = 50911 THEN valuenum ELSE NULL END) AS ck_mb
+  , MAX(CASE WHEN itemid = 50963 THEN valuenum ELSE NULL END) AS ntprobnp
 FROM mimic_hosp.labevents le
 WHERE le.itemid IN
 (
     -- 51002, -- Troponin I (troponin-I is not measured in MIMIC-IV)
     -- 52598, -- Troponin I, point of care, rare/poor quality
     51003, -- Troponin T
-    50911  -- Creatinine Kinase, MB isoenzyme
+    50911,  -- Creatinine Kinase, MB isoenzyme
+    50963 -- N-terminal (NT)-pro hormone BNP (NT-proBNP) 
 )
+AND valuenum IS NOT NULL
 GROUP BY le.specimen_id
 ;
