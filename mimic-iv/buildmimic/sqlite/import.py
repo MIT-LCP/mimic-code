b03184a20d51c540b150266c95068898777152b7
Alistair Johnson <alistairewj@gmail.com>
2022-07-07 19:04:56 -0400

    reformat using yapf

--- mimic-iv/buildmimic/sqlite/import.py
+++ mimic-iv/buildmimic/sqlite/import.py
@@ -4,10 +4,9 @@ import sys
 from glob import glob
 import pandas as pd
 
-
 DATABASE_NAME = "mimic4.db"
-THRESHOLD_SIZE = 5 * 10 ** 7
-CHUNKSIZE = 10 ** 6
+THRESHOLD_SIZE = 5 * 10**7
+CHUNKSIZE = 10**6
 CONNECTION_STRING = "sqlite:///{}".format(DATABASE_NAME)
 
 if os.path.exists(DATABASE_NAME):
@@ -15,9 +14,9 @@ if os.path.exists(DATABASE_NAME):
     print(msg)
     sys.exit()
 
-for f in glob("**/*.csv.gz",recursive=True):
+for f in glob("**/*.csv.gz", recursive=True):
     print("Starting processing {}".format(f))
-    folder,filename = os.path.split(f)
+    folder, filename = os.path.split(f)
     tablename = filename.strip(".csv.gz").lower()
     if os.path.getsize(f) < THRESHOLD_SIZE:
         df = pd.read_csv(f)
@@ -25,9 +24,7 @@ for f in glob("**/*.csv.gz",recursive=True):
     else:
         # If the file is too large, let's do the work in chunks
         for chunk in pd.read_csv(f, chunksize=CHUNKSIZE, low_memory=False):
-            chunk.to_sql(
-                tablename, CONNECTION_STRING, if_exists="append"
-            )
+            chunk.to_sql(tablename, CONNECTION_STRING, if_exists="append")
     print("Finished processing {}".format(f))
 
 print("Should be all done!")
