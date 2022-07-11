b24cb8e3dfe2344759917aa99c6ebb2ddc9953e5
Alistair Johnson <alistairewj@gmail.com>
2022-07-10 21:55:50 -0400

    mimic_ prefix -> mimiciv_

--- mimic-iv/buildmimic/postgres/load_7z.sql
+++ mimic-iv/buildmimic/postgres/load_7z.sql
@@ -12,36 +12,36 @@ SET CLIENT_ENCODING TO 'utf8';
 -- hosp schema
 \cd hosp
 
-\COPY mimic_hosp.admissions FROM PROGRAM '7z e -so admissions.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.d_hcpcs FROM PROGRAM '7z e -so d_hcpcs.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.diagnoses_icd FROM PROGRAM '7z e -so diagnoses_icd.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.d_icd_diagnoses FROM PROGRAM '7z e -so d_icd_diagnoses.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.d_icd_procedures FROM PROGRAM '7z e -so d_icd_procedures.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.d_labitems FROM PROGRAM '7z e -so d_labitems.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.drgcodes FROM PROGRAM '7z e -so drgcodes.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.emar_detail FROM PROGRAM '7z e -so emar_detail.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.emar FROM PROGRAM '7z e -so emar.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.hcpcsevents FROM PROGRAM '7z e -so hcpcsevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.labevents FROM PROGRAM '7z e -so labevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.microbiologyevents FROM PROGRAM '7z e -so microbiologyevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.omr FROM PROGRAM '7z e -so omr.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.patients FROM PROGRAM '7z e -so patients.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.pharmacy FROM PROGRAM '7z e -so pharmacy.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.poe_detail FROM PROGRAM '7z e -so poe_detail.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.poe FROM PROGRAM '7z e -so poe.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.prescriptions FROM PROGRAM '7z e -so prescriptions.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.procedures_icd FROM PROGRAM '7z e -so procedures_icd.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.services FROM PROGRAM '7z e -so services.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_hosp.transfers FROM PROGRAM '7z e -so transfers.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.admissions FROM PROGRAM '7z e -so admissions.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.d_hcpcs FROM PROGRAM '7z e -so d_hcpcs.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.diagnoses_icd FROM PROGRAM '7z e -so diagnoses_icd.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.d_icd_diagnoses FROM PROGRAM '7z e -so d_icd_diagnoses.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.d_icd_procedures FROM PROGRAM '7z e -so d_icd_procedures.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.d_labitems FROM PROGRAM '7z e -so d_labitems.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.drgcodes FROM PROGRAM '7z e -so drgcodes.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.emar_detail FROM PROGRAM '7z e -so emar_detail.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.emar FROM PROGRAM '7z e -so emar.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.hcpcsevents FROM PROGRAM '7z e -so hcpcsevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.labevents FROM PROGRAM '7z e -so labevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.microbiologyevents FROM PROGRAM '7z e -so microbiologyevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.omr FROM PROGRAM '7z e -so omr.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.patients FROM PROGRAM '7z e -so patients.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.pharmacy FROM PROGRAM '7z e -so pharmacy.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.poe_detail FROM PROGRAM '7z e -so poe_detail.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.poe FROM PROGRAM '7z e -so poe.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.prescriptions FROM PROGRAM '7z e -so prescriptions.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.procedures_icd FROM PROGRAM '7z e -so procedures_icd.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.services FROM PROGRAM '7z e -so services.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_hosp.transfers FROM PROGRAM '7z e -so transfers.csv.gz' DELIMITER ',' CSV HEADER NULL '';
 
 -- icu schema
 \cd ../icu
 
-\COPY mimic_icu.chartevents FROM PROGRAM '7z e -so chartevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_icu.datetimeevents FROM PROGRAM '7z e -so datetimeevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_icu.d_items FROM PROGRAM '7z e -so d_items.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_icu.icustays FROM PROGRAM '7z e -so icustays.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_icu.ingredientevents FROM PROGRAM '7z e -so ingredientevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_icu.inputevents FROM PROGRAM '7z e -so inputevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_icu.outputevents FROM PROGRAM '7z e -so outputevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
-\COPY mimic_icu.procedureevents FROM PROGRAM '7z e -so procedureevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_icu.chartevents FROM PROGRAM '7z e -so chartevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_icu.datetimeevents FROM PROGRAM '7z e -so datetimeevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_icu.d_items FROM PROGRAM '7z e -so d_items.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_icu.icustays FROM PROGRAM '7z e -so icustays.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_icu.ingredientevents FROM PROGRAM '7z e -so ingredientevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_icu.inputevents FROM PROGRAM '7z e -so inputevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_icu.outputevents FROM PROGRAM '7z e -so outputevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+\COPY mimiciv_icu.procedureevents FROM PROGRAM '7z e -so procedureevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
