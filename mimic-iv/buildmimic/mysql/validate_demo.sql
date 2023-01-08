5dc6117100ce2a4e42d8a6c9c05ca0a8f295c399
Alistair Johnson <alistairewj@gmail.com>
2023-01-07 21:18:01 -0500

    update row counts in validate scripts

--- mimic-iv/buildmimic/mysql/validate_demo.sql
+++ mimic-iv/buildmimic/mysql/validate_demo.sql
@@ -16,7 +16,7 @@ FROM (
     SELECT 'd_hcpcs' AS tbl, 89200 AS row_count UNION ALL
     SELECT 'd_icd_diagnoses' AS tbl, 109775 AS row_count UNION ALL
     SELECT 'd_icd_procedures' AS tbl, 85257 AS row_count UNION ALL
-    SELECT 'd_labitems' AS tbl, 1623 AS row_count UNION ALL
+    SELECT 'd_labitems' AS tbl, 1622 AS row_count UNION ALL
     SELECT 'diagnoses_icd' AS tbl, 4506 AS row_count UNION ALL
     SELECT 'drgcodes' AS tbl, 454 AS row_count UNION ALL
     SELECT 'emar' AS tbl, 35835 AS row_count UNION ALL
@@ -28,7 +28,7 @@ FROM (
     SELECT 'patients' AS tbl, 100 AS row_count UNION ALL
     SELECT 'pharmacy' AS tbl, 15306 AS row_count UNION ALL
     SELECT 'poe' AS tbl, 45154 AS row_count UNION ALL
-    SELECT 'poe_detail' AS tbl, 3053 AS row_count UNION ALL
+    SELECT 'poe_detail' AS tbl, 3795 AS row_count UNION ALL
     SELECT 'prescriptions' AS tbl, 18087 AS row_count UNION ALL
     SELECT 'procedures_icd' AS tbl, 722 AS row_count UNION ALL
     SELECT 'services' AS tbl, 319 AS row_count UNION ALL
