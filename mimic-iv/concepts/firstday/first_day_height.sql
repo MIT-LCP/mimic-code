b34340a370d480acb011bf25a2c16c9db85a6b5c
Alistair Johnson <alistairewj@gmail.com>
2023-02-08 14:03:11 -0500

    remove unused CTE

--- mimic-iv/concepts/firstday/first_day_height.sql
+++ mimic-iv/concepts/firstday/first_day_height.sql
@@ -1,23 +1,7 @@
 -- This query extracts heights for adult ICU patients.
 -- It uses all information from the patient's first ICU day.
--- This is done for consistency with other queries - it's not necessarily needed.
+-- This is done for consistency with other queries.
 -- Height is unlikely to change throughout a patient's stay.
-
--- The MIMIC-III version used echo data, this is not available in MIMIC-IV v0.4
-WITH ce AS (
-    SELECT
-        c.stay_id
-        , AVG(valuenum) AS height_chart
-    FROM `physionet-data.mimiciv_icu.chartevents` c
-    INNER JOIN `physionet-data.mimiciv_icu.icustays` ie
-        ON c.stay_id = ie.stay_id
-            AND c.charttime >= DATETIME_SUB(ie.intime, INTERVAL '1' DAY)
-            AND c.charttime <= DATETIME_ADD(ie.intime, INTERVAL '1' DAY)
-    WHERE c.valuenum IS NOT NULL
-        AND c.itemid IN (226730) -- height
-        AND c.valuenum != 0
-    GROUP BY c.stay_id
-)
 
 SELECT
     ie.subject_id
