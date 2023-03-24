a492c92fbdc9a284dba01b003efe6f48229860ae
Alistair Johnson <alistairewj@gmail.com>
2023-03-24 08:50:23 -0400

    add poe and change into a time/status table

--- mimic-iv/concepts/treatment/code_status.sql
+++ mimic-iv/concepts/treatment/code_status.sql
@@ -1,11 +1,9 @@
--- This query extracts:
---    i) a patient's first code status
---    ii) a patient's last code status
---    iii) the time of the first entry of DNR or CMO
+-- This query extracts code status with the time at which the
+-- code status was documented.
 
 WITH t1 AS (
   /*
-There are five distinct values for the code status order in the dataset:
+There are five distinct values for the code status order in the ICU data:
 1 DNR / DNI
 2	DNI (do not intubate)
 3	Comfort measures only
@@ -14,14 +12,10 @@ There are five distinct values for the code status order in the dataset:
  */
 
     SELECT
-        stay_id
+        subject_id
+        , hadm_id
+        , stay_id
         , charttime
-        , value
-        -- use row number to identify first and last code status
-        , ROW_NUMBER() OVER (PARTITION BY stay_id ORDER BY charttime) AS rnfirst
-        , ROW_NUMBER() OVER (
-            PARTITION BY stay_id ORDER BY charttime DESC
-        ) AS rnlast
         -- coalesce the values
         , CASE
             WHEN value IN ('Full code') THEN 1
@@ -39,43 +33,46 @@ There are five distinct values for the code status order in the dataset:
     WHERE itemid IN (223758)
 )
 
-SELECT
-    ie.subject_id
-    , ie.hadm_id
-    , ie.stay_id
-    -- first recorded code status
-    , MAX(
-        CASE WHEN rnfirst = 1 THEN t1.fullcode END
-    ) AS fullcode_first
-    , MAX(CASE WHEN rnfirst = 1 THEN t1.cmo END) AS cmo_first
-    , MAX(CASE WHEN rnfirst = 1 THEN t1.dnr END) AS dnr_first
-    , MAX(CASE WHEN rnfirst = 1 THEN t1.dni END) AS dni_first
+-- Provider order entry contains hospital wide orders for code status changes
+-- Interestingly, it does not contain comfort measures only orders
+, poe AS (
+    SELECT p.subject_id
+        , p.hadm_id
+        , ie.stay_id
+        , p.order_time
+        , CASE
+            WHEN pd.field_value = 'Resuscitate (Full code)' THEN 1
+            WHEN pd.field_value = 'Full code  (attempt resuscitation)' THEN 1
+            ELSE 0 END AS fullcode
+        , CASE
+            WHEN
 
-    -- last recorded code status
-    , MAX(
-        CASE WHEN rnlast = 1 THEN t1.fullcode END
-    ) AS fullcode_last
-    , MAX(CASE WHEN rnlast = 1 THEN t1.cmo END) AS cmo_last
-    , MAX(CASE WHEN rnlast = 1 THEN t1.dnr END) AS dnr_last
-    , MAX(CASE WHEN rnlast = 1 THEN t1.dni END) AS dni_last
+                pd.field_value = 'DNAR (DO NOT attempt resuscitation for cardiac arrest) ' THEN 1 -- noqa
+            WHEN pd.field_value = 'Do not resuscitate (DNR/DNI)' THEN 1
+            ELSE 0 END AS dnr
+        , CASE
+            WHEN pd.field_value = 'Do not resuscitate (DNR/DNI)' THEN 1
+            ELSE 0 END AS dni
+    FROM `physionet-data.mimiciv_hosp.poe` p
+    INNER JOIN `physionet-data.mimiciv_hosp.poe_detail` pd
+        ON p.poe_id = pd.poe_id
+    LEFT JOIN `physiont-data.mimiciv_icu.icustays` ie
+        ON p.hadm_id = ie.hadm_id
+            AND p.order_time >= ie.intime
+            AND p.order_time <= ie.outtime
+    WHERE p.order_type = 'General Care'
+        AND order_subtype = 'Code status'
+)
 
-    -- were they *at any time* given a certain code status
-    , MAX(t1.fullcode) AS fullcode
-    , MAX(t1.cmo) AS cmo
-    , MAX(t1.dnr) AS dnr
-    , MAX(t1.dni) AS dni
-
-    -- time until their first DNR
-    , MIN(CASE WHEN t1.dnr = 1 THEN t1.charttime END)
-    AS dnr_first_charttime
-    , MIN(CASE WHEN t1.dni = 1 THEN t1.charttime END)
-    AS dni_first_charttime
-
-    -- first code status of CMO
-    , MIN(CASE WHEN t1.cmo = 1 THEN t1.charttime END)
-    AS timecmo_chart
-
-FROM `physionet-data.mimic_icu.icustays` AS ie
-LEFT JOIN t1
-          ON ie.stay_id = t1.stay_id
-GROUP BY ie.subject_id, ie.hadm_id, ie.stay_id, ie.intime;
+-- Merge together code status from ICU data
+-- with code status from provider order entry
+SELECT t1.subject_id, t1.hadm_id, t1.stay_id
+    , t1.charttime
+    , t1.fullcode, t1.cmo, t1.dni, t1.dnr
+FROM t1
+UNION ALL
+SELECT poe.subject_id, poe.hadm_id, poe.stay_id
+    , poe.order_time AS charttime
+    , poe.fullcode, 0 AS cmo, poe.dni, poe.dnr
+FROM poe
+;
