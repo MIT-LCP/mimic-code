4eb302fe1068b4603e77d4c1f3ac485f99abf823
SphtKr <fthasphtkr@gmail.com>
2023-04-26 11:33:08 +0000

    Simplify pathing, strip more dead code

--- mimic-iii/buildmimic/duckdb/duckdb_concepts.py
+++ mimic-iii/buildmimic/duckdb/duckdb_concepts.py
@@ -18,94 +18,94 @@ from sqlglot.helper import seq_get
 from pprint import pprint
 
 concept_name_map = {
-    'icustay_times': {"path": "../../concepts/demographics/icustay_times.sql", "db": "bigquery"},
-    'icustay_hours': {"path": "./concepts/icustay_hours.sql", "db": "duckdb"},
-    'echo_data': {"path": "../../concepts/echo_data.sql", "db": "bigquery"},
-    'code_status': {"path": "../../concepts/code_status.sql", "db": "bigquery"},
-    'weight_durations': {"path": "../../concepts/durations/weight_durations.sql", "db": "bigquery"},
-    'rrt': {"path": "../../concepts/rrt.sql", "db": "bigquery"},
-    'heightweight': {"path": "../../concepts/demographics/heightweight.sql", "db": "bigquery"},
-    'icustay_detail': {"path": "../../concepts/demographics/icustay_detail.sql", "db": "bigquery"},
+    'icustay_times': {"path": "demographics/icustay_times.sql"},
+    'icustay_hours': {"path": "icustay_hours.sql", "db": "duckdb"},
+    'echo_data': {"path": "echo_data.sql"},
+    'code_status': {"path": "code_status.sql"},
+    'weight_durations': {"path": "durations/weight_durations.sql"},
+    'rrt': {"path": "rrt.sql"},
+    'heightweight': {"path": "demographics/heightweight.sql"},
+    'icustay_detail': {"path": "demographics/icustay_detail.sql"},
 
-    'ventilation_classification': {"path": "../../concepts/durations/ventilation_classification.sql", "db": "bigquery"},
-    'ventilation_durations': {"path": "../../concepts/durations/ventilation_durations.sql", "db": "bigquery"},
-    'crrt_durations': {"path": "../../concepts/durations/crrt_durations.sql", "db": "bigquery"},
-    'adenosine_durations': {"path": "../../concepts/durations/adenosine_durations.sql", "db": "bigquery"},
-    'dobutamine_durations': {"path": "../../concepts/durations/dobutamine_durations.sql", "db": "bigquery"},
-    'dopamine_durations': {"path": "../../concepts/durations/dopamine_durations.sql", "db": "bigquery"},
-    'epinephrine_durations': {"path": "../../concepts/durations/epinephrine_durations.sql", "db": "bigquery"},
-    'isuprel_durations': {"path": "../../concepts/durations/isuprel_durations.sql", "db": "bigquery"},
-    'milrinone_durations': {"path": "../../concepts/durations/milrinone_durations.sql", "db": "bigquery"},
-    'norepinephrine_durations': {"path": "../../concepts/durations/norepinephrine_durations.sql", "db": "bigquery"},
-    'phenylephrine_durations': {"path": "../../concepts/durations/phenylephrine_durations.sql", "db": "bigquery"},
-    'vasopressin_durations': {"path": "../../concepts/durations/vasopressin_durations.sql", "db": "bigquery"},
-    'vasopressor_durations': {"path": "../../concepts/durations/vasopressor_durations.sql", "db": "bigquery"},
+    'ventilation_classification': {"path": "durations/ventilation_classification.sql"},
+    'ventilation_durations': {"path": "durations/ventilation_durations.sql"},
+    'crrt_durations': {"path": "durations/crrt_durations.sql"},
+    'adenosine_durations': {"path": "durations/adenosine_durations.sql"},
+    'dobutamine_durations': {"path": "durations/dobutamine_durations.sql"},
+    'dopamine_durations': {"path": "durations/dopamine_durations.sql"},
+    'epinephrine_durations': {"path": "durations/epinephrine_durations.sql"},
+    'isuprel_durations': {"path": "durations/isuprel_durations.sql"},
+    'milrinone_durations': {"path": "durations/milrinone_durations.sql"},
+    'norepinephrine_durations': {"path": "durations/norepinephrine_durations.sql"},
+    'phenylephrine_durations': {"path": "durations/phenylephrine_durations.sql"},
+    'vasopressin_durations': {"path": "durations/vasopressin_durations.sql"},
+    'vasopressor_durations': {"path": "durations/vasopressor_durations.sql"},
 
-    'dobutamine_dose': {"path": "../../concepts/durations/dobutamine_dose.sql", "db": "bigquery"},
-    'dopamine_dose': {"path": "../../concepts/durations/dopamine_dose.sql", "db": "bigquery"},
-    'epinephrine_dose': {"path": "../../concepts/durations/epinephrine_dose.sql", "db": "bigquery"},
-    'norepinephrine_dose': {"path": "../../concepts/durations/norepinephrine_dose.sql", "db": "bigquery"},
-    'phenylephrine_dose': {"path": "../../concepts/durations/phenylephrine_dose.sql", "db": "bigquery"},
-    'vasopressin_dose': {"path": "../../concepts/durations/vasopressin_dose.sql", "db": "bigquery"},
+    'dobutamine_dose': {"path": "durations/dobutamine_dose.sql"},
+    'dopamine_dose': {"path": "durations/dopamine_dose.sql"},
+    'epinephrine_dose': {"path": "durations/epinephrine_dose.sql"},
+    'norepinephrine_dose': {"path": "durations/norepinephrine_dose.sql"},
+    'phenylephrine_dose': {"path": "durations/phenylephrine_dose.sql"},
+    'vasopressin_dose': {"path": "durations/vasopressin_dose.sql"},
 
-    'pivoted_vital': {"path": "../../concepts/pivot/pivoted_vital.sql", "db": "bigquery"},
-    'pivoted_uo': {"path": "../../concepts/pivot/pivoted_uo.sql", "db": "bigquery"},
-    'pivoted_rrt': {"path": "../../concepts/pivot/pivoted_rrt.sql", "db": "bigquery"},
-    'pivoted_lab': {"path": "../../concepts/pivot/pivoted_lab.sql", "db": "bigquery"},
-    'pivoted_invasive_lines': {"path": "../../concepts/pivot/pivoted_invasive_lines.sql", "db": "bigquery"},
-    'pivoted_icp': {"path": "../../concepts/pivot/pivoted_icp.sql", "db": "bigquery"},
-    'pivoted_height': {"path": "../../concepts/pivot/pivoted_height.sql", "db": "bigquery"},
-    'pivoted_gcs': {"path": "../../concepts/pivot/pivoted_gcs.sql", "db": "bigquery"},
-    'pivoted_fio2': {"path": "../../concepts/pivot/pivoted_fio2.sql", "db": "bigquery"},
-    'pivoted_bg': {"path": "../../concepts/pivot/pivoted_bg.sql", "db": "bigquery"},
+    'pivoted_vital': {"path": "pivot/pivoted_vital.sql"},
+    'pivoted_uo': {"path": "pivot/pivoted_uo.sql"},
+    'pivoted_rrt': {"path": "pivot/pivoted_rrt.sql"},
+    'pivoted_lab': {"path": "pivot/pivoted_lab.sql"},
+    'pivoted_invasive_lines': {"path": "pivot/pivoted_invasive_lines.sql"},
+    'pivoted_icp': {"path": "pivot/pivoted_icp.sql"},
+    'pivoted_height': {"path": "pivot/pivoted_height.sql"},
+    'pivoted_gcs': {"path": "pivot/pivoted_gcs.sql"},
+    'pivoted_fio2': {"path": "pivot/pivoted_fio2.sql"},
+    'pivoted_bg': {"path": "pivot/pivoted_bg.sql"},
     # pivoted_bg_art must be run after pivoted_bg
-    'pivoted_bg_art': {"path": "../../concepts/pivot/pivoted_bg_art.sql", "db": "bigquery"},
+    'pivoted_bg_art': {"path": "pivot/pivoted_bg_art.sql"},
     # Difficult error here, the original query seems to reference something non-existent...
     # the `pivot` queries are omitted from the Postgres version... we may have to do the same?
     # pivoted oasis depends on icustay_hours in demographics
-    #'pivoted_oasis': {"path": "../../concepts/pivot/pivoted_oasis.sql", "db": "bigquery"},
+    #'pivoted_oasis': {"path": "pivot/pivoted_oasis.sql"},
     # Another puzzling error here, duckdb doesn't like something on the `WITH` line!
     # pivoted sofa depends on many above pivoted views, ventilation_durations, and dose queries
-    #'pivoted_sofa': {"path": "../../concepts/pivot/pivoted_sofa.sql", "db": "bigquery"},
+    #'pivoted_sofa': {"path": "pivot/pivoted_sofa.sql"},
 
-    'elixhauser_ahrq_v37': {"path": "../../concepts/comorbidity/elixhauser_ahrq_v37.sql", "db": "bigquery"},
-    'elixhauser_ahrq_v37_no_drg': {"path": "../../concepts/comorbidity/elixhauser_ahrq_v37_no_drg.sql", "db": "bigquery"},
-    'elixhauser_quan': {"path": "../../concepts/comorbidity/elixhauser_quan.sql", "db": "bigquery"},
-    'elixhauser_score_ahrq': {"path": "../../concepts/comorbidity/elixhauser_score_ahrq.sql", "db": "bigquery"},
-    'elixhauser_score_quan': {"path": "../../concepts/comorbidity/elixhauser_score_quan.sql", "db": "bigquery"},
+    'elixhauser_ahrq_v37': {"path": "comorbidity/elixhauser_ahrq_v37.sql"},
+    'elixhauser_ahrq_v37_no_drg': {"path": "comorbidity/elixhauser_ahrq_v37_no_drg.sql"},
+    'elixhauser_quan': {"path": "comorbidity/elixhauser_quan.sql"},
+    'elixhauser_score_ahrq': {"path": "comorbidity/elixhauser_score_ahrq.sql"},
+    'elixhauser_score_quan': {"path": "comorbidity/elixhauser_score_quan.sql"},
 
-    'blood_gas_first_day': {"path": "../../concepts/firstday/blood_gas_first_day.sql", "db": "bigquery"},
-    'blood_gas_first_day_arterial': {"path": "../../concepts/firstday/blood_gas_first_day_arterial.sql", "db": "bigquery"},
-    'gcs_first_day': {"path": "../../concepts/firstday/gcs_first_day.sql", "db": "bigquery"},
-    'labs_first_day': {"path": "../../concepts/firstday/labs_first_day.sql", "db": "bigquery"},
-    'rrt_first_day': {"path": "../../concepts/firstday/rrt_first_day.sql", "db": "bigquery"},
-    'urine_output_first_day': {"path": "../../concepts/firstday/urine_output_first_day.sql", "db": "bigquery"},
-    'ventilation_first_day': {"path": "../../concepts/firstday/ventilation_first_day.sql", "db": "bigquery"},
-    'vitals_first_day': {"path": "../../concepts/firstday/vitals_first_day.sql", "db": "bigquery"},
-    'weight_first_day': {"path": "../../concepts/firstday/weight_first_day.sql", "db": "bigquery"},
+    'blood_gas_first_day': {"path": "firstday/blood_gas_first_day.sql"},
+    'blood_gas_first_day_arterial': {"path": "firstday/blood_gas_first_day_arterial.sql"},
+    'gcs_first_day': {"path": "firstday/gcs_first_day.sql"},
+    'labs_first_day': {"path": "firstday/labs_first_day.sql"},
+    'rrt_first_day': {"path": "firstday/rrt_first_day.sql"},
+    'urine_output_first_day': {"path": "firstday/urine_output_first_day.sql"},
+    'ventilation_first_day': {"path": "firstday/ventilation_first_day.sql"},
+    'vitals_first_day': {"path": "firstday/vitals_first_day.sql"},
+    'weight_first_day': {"path": "firstday/weight_first_day.sql"},
     
-    'urine_output': {"path": "../../concepts/fluid_balance/urine_output.sql", "db": "bigquery"},
+    'urine_output': {"path": "fluid_balance/urine_output.sql"},
 
-    'angus': {"path": "../../concepts/sepsis/angus.sql", "db": "bigquery"},
-    'martin': {"path": "../../concepts/sepsis/martin.sql", "db": "bigquery"},
-    'explicit': {"path": "../../concepts/sepsis/explicit.sql", "db": "bigquery"},
+    'angus': {"path": "sepsis/angus.sql"},
+    'martin': {"path": "sepsis/martin.sql"},
+    'explicit': {"path": "sepsis/explicit.sql"},
 
-    'ccs_dx': {"path": "../../concepts/diagnosis/ccs_dx.sql", "db": "bigquery"},
+    'ccs_dx': {"path": "diagnosis/ccs_dx.sql"},
 
-    'kdigo_creatinine': {"path": "../../concepts/organfailure/kdigo_creatinine.sql", "db": "bigquery"},
-    'kdigo_uo': {"path": "../../concepts/organfailure/kdigo_uo.sql", "db": "bigquery"},
-    'kdigo_stages': {"path": "../../concepts/organfailure/kdigo_stages.sql", "db": "bigquery"},
-    'kdigo_stages_7day': {"path": "../../concepts/organfailure/kdigo_stages_7day.sql", "db": "bigquery"},
-    'kdigo_stages_48hr': {"path": "../../concepts/organfailure/kdigo_stages_48hr.sql", "db": "bigquery"},
-    'meld': {"path": "../../concepts/organfailure/meld.sql", "db": "bigquery"},
+    'kdigo_creatinine': {"path": "organfailure/kdigo_creatinine.sql"},
+    'kdigo_uo': {"path": "organfailure/kdigo_uo.sql"},
+    'kdigo_stages': {"path": "organfailure/kdigo_stages.sql"},
+    'kdigo_stages_7day': {"path": "organfailure/kdigo_stages_7day.sql"},
+    'kdigo_stages_48hr': {"path": "organfailure/kdigo_stages_48hr.sql"},
+    'meld': {"path": "organfailure/meld.sql"},
 
-    'oasis': {"path": "../../concepts/severityscores/oasis.sql", "db": "bigquery"},
-    'sofa': {"path": "../../concepts/severityscores/sofa.sql", "db": "bigquery"},
-    'saps': {"path": "../../concepts/severityscores/saps.sql", "db": "bigquery"},
-    'sapsii': {"path": "../../concepts/severityscores/sapsii.sql", "db": "bigquery"},
-    'apsiii': {"path": "../../concepts/severityscores/apsiii.sql", "db": "bigquery"},
-    'lods': {"path": "../../concepts/severityscores/lods.sql", "db": "bigquery"},
-    'sirs': {"path": "../../concepts/severityscores/sirs.sql", "db": "bigquery"},
+    'oasis': {"path": "severityscores/oasis.sql"},
+    'sofa': {"path": "severityscores/sofa.sql"},
+    'saps': {"path": "severityscores/saps.sql"},
+    'sapsii': {"path": "severityscores/sapsii.sql"},
+    'apsiii': {"path": "severityscores/apsiii.sql"},
+    'lods': {"path": "severityscores/lods.sql"},
+    'sirs': {"path": "severityscores/sirs.sql"},
 
 }
 
@@ -136,21 +136,16 @@ sqlglot.dialects.duckdb.DuckDB.Generator.TRANSFORMS[exp.DatetimeDiff] = duckdb_d
 sqlglot.dialects.duckdb.DuckDB.Generator.TRANSFORMS[exp.DateDiff] = duckdb_date_diff_sql
 
 
-def _make_duckdb_query_bigquery(qname: str, conn):
+def _make_duckdb_query_bigquery(qname: str, qfile: str, conn):
     _multischema_trunc_re = re.compile("\"physionet-data\.mimiciii_\w+\.")
     
     #TODO: better answer here? should only hit ccs_dx.sql!
-    _too_many_backslashes_re = re.compile("\\\\([\[\.\]])", ) 
+    _too_many_backslashes_re = re.compile("\\\\([\[\.\]])") 
 
-    #_whole_line_comments_strip_re = re.compile("^\s*--.*$", flags=re.MULTILINE)
-    qfile = concept_name_map[qname]["path"]
     with open(qfile, "r") as fp:
         sql = fp.read()
-        ##strip comments manually... one weird thing happening in pivoted_sofa...??
-        #sql = re.sub(_whole_line_comments_strip_re, '', sql)
         sql = re.sub(_too_many_backslashes_re, '\\$1', sql) 
         try:
-            #print(repr(sqlglot.parse_one(sql.replace('`','"'))))
             sql_list = sqlglot.transpile(sql, read="bigquery", write="duckdb", pretty=True)
         except Exception as e:
             print(sql)
@@ -158,10 +153,6 @@ def _make_duckdb_query_bigquery(qname: str, conn):
         print()
         for st in sql_list:
             sql = re.sub(_multischema_trunc_re, "\"", st)
-            #ast = sqlglot.parse_one(sql)
-            #ast2 = ast.transform(_bigquery_duckdb_transformer)
-            #sql = ast2.sql(pretty=True)
-            #print(sql)
 
             if concept_name_map[qname].get("nocreate", False):
                 cursor = conn.cursor()
@@ -188,8 +179,7 @@ def _make_duckdb_query_bigquery(qname: str, conn):
         #print()
 
 
-def _make_duckdb_query_duckdb(qname: str, conn):
-    qfile = concept_name_map[qname]["path"]
+def _make_duckdb_query_duckdb(qname: str, qfile: str, conn):
     with open(qfile, "r") as fp:
         sql = fp.read()
         if concept_name_map[qname].get("nocreate", False):
@@ -283,13 +273,13 @@ def main() -> int:
             for key in concept_name_map:
                 #cProfile.run('...')
                 #print(f"Making view {key}...")
-                db = concept_name_map[key].get("db", "postgres")
+                db = concept_name_map[key].get("db", "bigquery")
                 if db == "duckdb":
-                    _make_duckdb_query_duckdb(key, connection)
+                    qpath = os.path.join(mimic_code_root, 'mimic-iii', 'buildmimic', 'duckdb', 'concepts', concept_name_map[key]['path'])
+                    _make_duckdb_query_duckdb(key, qpath, connection)
                 elif db == "bigquery":
-                    _make_duckdb_query_bigquery(key, connection)
-                elif db == "postgres":
-                    _make_duckdb_query_postgres(key, connection)
+                    qpath = os.path.join(mimic_code_root, 'mimic-iii', 'concepts', concept_name_map[key]['path'])
+                    _make_duckdb_query_bigquery(key, qpath, connection)
 
         except Exception as error:
             print("Failed to execute translated SQL: ", error)
